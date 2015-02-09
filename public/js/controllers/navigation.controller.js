9c24a1a6d1c7cf8faee14f8a8f7f5f9587b668a4
Luke Vella <me@lukevella>
2015-02-09 16:10:47 +0100

    Mobile Support

--- public/js/controllers/navigation.controller.js
+++ public/js/controllers/navigation.controller.js
@@ -1,5 +1,5 @@
 angular.module('rallly')
-.controller('NavigationCtrl', function($scope, $location){
+.controller('NavigationCtrl', function($scope, $rootScope, $location, $document){
     $scope.isActive = function(path) {
         if ($location.path() == path) {
             return true;
@@ -7,4 +7,31 @@ angular.module('rallly')
             return false
         }
     }
+
+
+
+    var clickHandler = function(event){
+        var isClickedElementChildOfPopup = angular.element('.main-navigation').find(event.target).length > 0;
+        console.log(isClickedElementChildOfPopup);
+        if (isClickedElementChildOfPopup) return;
+        $scope.toggleMenu()
+    }
+
+    $rootScope.$on('$stateChangeSuccess', function(){
+        angular.element('body').removeClass('open-menu');
+        $document.unbind('click', clickHandler);
+    });
+
+    $scope.toggleMenu = function(){
+        var isOpen = angular.element('body').hasClass('open-menu');
+        if (isOpen) {
+            angular.element('body').removeClass('open-menu');
+            $document.unbind('click', clickHandler);
+        } else {
+            angular.element('body').addClass('open-menu');
+            $document.bind('click', clickHandler);
+        }
+    }
+
+
 })
