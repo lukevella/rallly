96d248575d0d1e3700aca0611a142ddcbb42a008
Luke Vella <me@lukevella>
2015-02-11 12:17:29 +0100

    Landin Page

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,19 @@
 module.exports = function(app) {
 
-    // Insert routes below
+    app.get('/', function(req,res){
+        res.render('index');
+    });
+
     app.use('/api/event', require('../api/event'));
     // All undefined asset or api routes should return a 404
     // app.route('/:url(api|auth|components|app|bower_components|assets)/*')
     // .get(errors[404]);
-    
+
     // All other routes should redirect to the
+
     app.route('/*')
     .get(function(req, res) {
-        res.render('index.ejs');
+        res.render('app');
     });
 
     if (app.get('env') === 'development') {
