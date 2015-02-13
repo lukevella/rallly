a703b8e3c9004b655cd53d523b871f86c596ee63
Luke Vella <me@lukevella>
2015-02-13 11:55:41 +0100

    Created install script and added comments

--- config/main.sample.js
+++ config/main.sample.js
@@ -1,11 +1,11 @@
 module.exports = function(app){
     app.set('port', 3000);
-    app.set('siteUrl', '');
+    app.set('siteUrl', ''); // Used for creating an absolute URL
     app.set('absoluteUrl', function(path){
         return app.get('siteUrl') + ':' + app.get('port') + '/' + path;
     });
-    app.set('dbname', '');
-    app.set('dbuser', '');
-    app.set('dbpwd', '');
-    app.set('mandrillAPIKey','');
+    app.set('dbname', ''); // MongoDB database name
+    app.set('dbuser', ''); // MongoDB user name
+    app.set('dbpwd', ''); // MongoDB user password
+    app.set('mandrillAPIKey',''); // https://mandrillapp.com
 };
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+npm install
+bower install
+cp config/main.sample.js config/main.js
