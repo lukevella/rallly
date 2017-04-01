37774477ea8ba9e63857c10ffceace81bfd3974d
Noah Prail <nprail@filiosoft.com>
2017-04-01 09:49:38 -0400

    New config options. From name, from email address, and sendgrid template id. #29

--- config/main.sample.js
+++ config/main.sample.js
@@ -1,11 +1,14 @@
-module.exports = function(app){
+module.exports = function (app) {
     app.set('port', 3000);
-    app.set('siteUrl', ''); // Used for creating an absolute URL
-    app.set('absoluteUrl', function(path){
-        return app.get('siteUrl') + ':' + app.get('port') + '/' + path;
+    app.set('siteUrl', ''); // Used for creating an absolute URL - include port
+    app.set('absoluteUrl', function (path) {
+        return app.get('siteUrl') + '/' + path;
     });
+    app.set('fromName', 'Rallly'); // Email from name
+    app.set('fromEmail', 'no-reply@rally.co'); // Email from address
     app.set('dbname', ''); // MongoDB database name
-    app.set('dbuser', ''); // MongoDB user name
-    app.set('dbpwd', ''); // MongoDB user password
-    app.set('sendGridAPIKey',''); // https://sendgrid.com
+    app.set('dbuser', ''); // MongoDB user name - can be blank if authentication is disabled
+    app.set('dbpwd', ''); // MongoDB user password - can be blank if authentication is disabled
+    app.set('sendGridAPIKey', ''); // https://sendgrid.com
+    app.set('sendGridTemplateId', ''); // https://sendgrid.com
 };
--- helpers/notification.helper.js
+++ helpers/notification.helper.js
@@ -7,120 +7,120 @@ var communicator = require('../communicator');
 var debug = require('debug')('rallly');
 var sendgrid = require('sendgrid')(app.get('sendGridAPIKey'));
 
-var getEmail = function(options) {
-  var email =  new sendgrid.Email({
-    from : 'noreply@rallly.co',
-    fromname : 'Rallly'
-  });
-  email.addTo(options.to);
-  email.setSubject(options.subject);
-  email.setHtml(options.message);
-  email.addSubstitution('[title]', options.title);
-  email.addSubstitution('[buttontext]', options.buttonText);
-  email.addSubstitution('[buttonurl]', options.buttonURL);
-  email.addFilter('templates', 'enable', 1);
-  email.addFilter('templates', 'template_id', '579b31d6-3a04-4f5b-b0de-b57abd5d27d7');
-  return email;
+var getEmail = function (options) {
+    var email = new sendgrid.Email({
+        from: app.get('fromEmail'),
+        fromname: app.get('fromName')
+    });
+    email.addTo(options.to);
+    email.setSubject(options.subject);
+    email.setHtml(options.message);
+    email.addSubstitution('[title]', options.title);
+    email.addSubstitution('[buttontext]', options.buttonText);
+    email.addSubstitution('[buttonurl]', options.buttonURL);
+    email.addFilter('templates', 'enable', 1);
+    email.addFilter('templates', 'template_id', app.get('sendGridTemplateId'));
+    return email;
 };
 
-var responseHandler = function(err, json) {
-  if (err) {
-    debug('A mandrill error occurred: ' + err.name + ' - ' + err.message);
-  }
+var responseHandler = function (err, json) {
+    if (err) {
+        debug('A mandrill error occurred: ' + err.name + ' - ' + err.message);
+    }
 }
 
-communicator.on('event:create', function(event){
+communicator.on('event:create', function (event) {
     sendInvites(event);
     if (!event.creator.allowNotifications || event.isClosed || event.isExample) return;
     sendEmailConfirmation(event);
 });
 
-communicator.on('event:update:creator.email', function(event, oldEvent){
+communicator.on('event:update:creator.email', function (event, oldEvent) {
     if (!event.creator.allowNotifications || event.isClosed || event.isExample) return;
     verifyEmail(event);
 });
 
-communicator.on('participant:add', function(event, participant){
+communicator.on('participant:add', function (event, participant) {
     if (!event.creator.allowNotifications || !event.creator.isVerified || event.isExample) return;
     sendNewParticipantNotification(event, participant);
 });
 
-communicator.on('comment:add', function(event, comment){
+communicator.on('comment:add', function (event, comment) {
     if (!event.creator.allowNotifications || !event.creator.isVerified || event.isExample) return;
     sendNewCommentNotification(event, comment);
 });
 
 // Send confirmation to the creator of the event with a link to verify the creators email address
-var sendEmailConfirmation = function(event){
+var sendEmailConfirmation = function (event) {
     var email = getEmail({
-      to: event.creator.email,
-      subject:  'Rallly: ' + event.title + ' - Verify Email Address',
-      title:    'Your event ' + event.title + ' has been created successfully.',
-      buttonText: 'Verify Email Address',
-      buttonURL:  app.get('absoluteUrl')('verify/'+event._id+'/code/'+event.__private.verificationCode),
-      message:  'Hi [name],<br /><br />' +
-                'An email has been sent to each participant with a link to the event.<br /><br />' +
-                'Important: To continue receiving email notifications about this event, please click the button below to verify your email address.'
+        to: event.creator.email,
+        subject: 'Rallly: ' + event.title + ' - Verify Email Address',
+        title: 'Your event ' + event.title + ' has been created successfully.',
+        buttonText: 'Verify Email Address',
+        buttonURL: app.get('absoluteUrl')('verify/' + event._id + '/code/' + event.__private.verificationCode),
+        message: 'Hi [name],<br /><br />' +
+            'An email has been sent to each participant with a link to the event.<br /><br />' +
+            'Important: To continue receiving email notifications about this event, please click the button below to verify your email address.'
     });
     email.addSubstitution('[name]', event.creator.name);
     sendgrid.send(email, responseHandler);
 }
 
 // Send an invite to all participants of the evnet
-var sendInvites = function(event){
-    event.emails.forEach(function(item) {
-      var email = getEmail({
-        to:         item.email,
-        subject:    'Rallly: ' + event.title,
-        title:      event.creator.name + ' has invited you to participate in their event: ' + event.title,
-        buttonText: 'View Event',
-        buttonURL:  app.get('absoluteUrl')(event._id),
-        message:    'Rallly is a free collaborative scheduling service that lets you and your friends vote on a date to host an event. ' +
-                    'Click on the button below to visit the event page and vote on the dates that best suit you.'
-      });
-      email.replyto = event.creator.email;
-      sendgrid.send(email, responseHandler);
+var sendInvites = function (event) {
+    event.emails.forEach(function (item) {
+        var email = getEmail({
+            to: item.email,
+            subject: 'Rallly: ' + event.title,
+            title: event.creator.name + ' has invited you to participate in their event: ' + event.title,
+            buttonText: 'View Event',
+            buttonURL: app.get('absoluteUrl')(event._id),
+            message: 'Rallly is a free collaborative scheduling service that lets you and your friends vote on a date to host an event. ' +
+                'Click on the button below to visit the event page and vote on the dates that best suit you.'
+        });
+        email.replyto = event.creator.email;
+        sendgrid.send(email, responseHandler);
     });
 }
 
 // This message is sent when the user want to verify an email address after the event has been created
-var verifyEmail = function(event){
+var verifyEmail = function (event) {
     var email = getEmail({
-      to: event.creator.email,
-      subject: 'Rallly: ' + event.title + ' - Verify Email Address',
-      title: 'Please verify your email address to receive updates from this event.',
-      buttonText: 'Verify Email Address',
-      buttonURL: app.get('absoluteUrl')('verify/'+event._id+'/code/'+event.__private.verificationCode),
-      message:  'Hi [name],<br /><br />' +
-                'If you would like to receive email updates from this event, please click on the button below to verify your email address.'
+        to: event.creator.email,
+        subject: 'Rallly: ' + event.title + ' - Verify Email Address',
+        title: 'Please verify your email address to receive updates from this event.',
+        buttonText: 'Verify Email Address',
+        buttonURL: app.get('absoluteUrl')('verify/' + event._id + '/code/' + event.__private.verificationCode),
+        message: 'Hi [name],<br /><br />' +
+            'If you would like to receive email updates from this event, please click on the button below to verify your email address.'
     });
     email.addSubstitution('[name]', event.creator.name);
     sendgrid.send(email, responseHandler);
 }
 
-var sendNewParticipantNotification = function(event, participant){
+var sendNewParticipantNotification = function (event, participant) {
     var email = getEmail({
-      to: event.creator.email,
-      subject: 'Rallly: ' + event.title + ' - New Partcipant',
-      title: participant.name + ' has voted!',
-      buttonText: 'View Event',
-      buttonURL: app.get('absoluteUrl')(event._id),
-      message:  'Hi [name],<br /><br />' +
-                'Click the button below to see the updates made to your event page!'
+        to: event.creator.email,
+        subject: 'Rallly: ' + event.title + ' - New Partcipant',
+        title: participant.name + ' has voted!',
+        buttonText: 'View Event',
+        buttonURL: app.get('absoluteUrl')(event._id),
+        message: 'Hi [name],<br /><br />' +
+            'Click the button below to see the updates made to your event page!'
     });
     email.addSubstitution('[name]', event.creator.name);
     sendgrid.send(email, responseHandler);
 }
 
-var sendNewCommentNotification = function(event, comment){
+var sendNewCommentNotification = function (event, comment) {
     var email = getEmail({
-      to: event.creator.email,
-      subject: 'Rallly: ' + event.title + ' - New Comment',
-      title: comment.author.name + ' has commented on your event!',
-      buttonText: 'View Event',
-      buttonURL: app.get('absoluteUrl')(event._id),
-      message:  'Hi [name],<br /><br />' +
-                'Click the button below to see the updates made to your event page!'
+        to: event.creator.email,
+        subject: 'Rallly: ' + event.title + ' - New Comment',
+        title: comment.author.name + ' has commented on your event!',
+        buttonText: 'View Event',
+        buttonURL: app.get('absoluteUrl')(event._id),
+        message: 'Hi [name],<br /><br />' +
+            'Click the button below to see the updates made to your event page!'
     });
     email.addSubstitution('[name]', event.creator.name);
     sendgrid.send(email, responseHandler);
