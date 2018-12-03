94495947f1618d6354ea12ee4c6b59e349068387
Luke Vella <me@lukevella.com>
2018-12-03 19:04:07 +0000

    Update dependencies

--- config/routes.js
+++ config/routes.js
@@ -22,13 +22,4 @@ module.exports = function(app) {
         res.render('app');
     });
 
-    if (app.get('env') === 'development') {
-        app.use(function(err, req, res, next) {
-            res.status(err.status || 500);
-            res.render('error.ejs', {
-                message: err.message,
-                error: err
-            });
-        });
-    }
 };
