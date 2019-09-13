7272cdcf527458a6bf72012d7056660dffb0cd69
Luke Vella <me@lukevella.com>
2019-09-13 23:57:11 +0100

    Rate limit all upstream requests

--- api/event/index.js
+++ api/event/index.js
@@ -20,16 +20,32 @@ router.post('/', rateLimit({
     message: 'Too many requests from your IP, please try again after an hour'
 }), controller.create, after);
 router.get('/:id', controller.show, after);
-router.put('/:id', controller.update, after);
+router.put('/:id', rateLimit({
+    windowMs: 60 * 1000,
+    max: 25,
+    message: 'Too many requests from your IP, please try again later'
+}), controller.update, after);
 router.delete('/:id', controller.delete, after);
 router.delete('/:id/code/:code', controller.delete, after);
 router.get('/:id/code/:code', controller.verifyEmail, after);
 
-router.post('/:id/comment', controller.createComment, after);
+router.post('/:id/comment', rateLimit({
+    windowMs: 60 * 1000, // 1 hour window
+    max: 10, // start blocking after 5 requests
+    message: 'Too many requests from your IP, please try again after an hour'
+}), controller.createComment, after);
 router.delete('/:id/comment/:cid', controller.deleteComment, after);
 
-router.post('/:id/participant', controller.createParticipant, after);
+router.post('/:id/participant', rateLimit({
+    windowMs: 60 * 1000, // 1 hour window
+    max: 10, // start blocking after 5 requests
+    message: 'Too many requests from your IP, please try again after an hour'
+}),  controller.createParticipant, after);
 router.delete('/:id/participant/:pid', controller.deleteParticipant, after);
-router.put('/:id/participant/:pid', controller.updateParticipant, after);
+router.put('/:id/participant/:pid', rateLimit({
+    windowMs: 60 * 1000, // 1 hour window
+    max: 10, // start blocking after 5 requests
+    message: 'Too many requests from your IP, please try again after an hour'
+}), controller.updateParticipant, after);
 
 module.exports = router;
