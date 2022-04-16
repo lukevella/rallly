cb8b94362ea5d9ffef80a74ac80075234e6fe697
Luke Vella <me@lukevella.com>
2022-04-16 09:20:03 +0100

    Update meta description on home page

--- components/home.tsx
+++ components/home.tsx
@@ -1,14 +1,18 @@
 import Head from "next/head";
 import React from "react";
-import PageLayout from "../page-layout";
-import Bonus from "./bonus";
-import Features from "./features";
-import Hero from "./hero";
+import PageLayout from "./page-layout";
+import Bonus from "./home/bonus";
+import Features from "./home/features";
+import Hero from "./home/hero";
 
 const Home: React.VoidFunctionComponent = () => {
   return (
     <PageLayout>
       <Head>
+        <meta
+          name="description"
+          content="Create polls and vote to find the best day or time. A free alternative to Doodle."
+        />
         <title>Rallly - Schedule group meetings</title>
       </Head>
       <Hero />
