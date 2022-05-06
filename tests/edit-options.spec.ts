82662b14db4f60547b54f2bbde80336601b93469
Luke Vella <me@lukevella.com>
2022-05-06 16:37:49 +0100

    Fix test

--- tests/edit-options.spec.ts
+++ tests/edit-options.spec.ts
@@ -12,7 +12,7 @@ test("should show warning when deleting options with votes in them", async ({
   await page.click("[data-testid='specify-times-switch']");
   await page.click("text='12:00 PM'");
   await page.click("text='1:00 PM'");
-  await page.click("text='Save'");
+  await page.locator("div[role='dialog']").locator("text='Save'").click();
   await expect(page.locator('text="Are you sure?"')).toBeVisible();
   await page.click("text='Delete'");
 });
