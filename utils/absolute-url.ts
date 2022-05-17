efdc6cb038678264cd592785aeab3bc004157da8
Luke Vella <me@lukevella.com>
2022-05-17 18:22:31 +0100

    Support vercel preview url

--- utils/absolute-url.ts
+++ utils/absolute-url.ts
@@ -1,5 +1,9 @@
+const getVercelUrl = () => {
+  return process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : null;
+};
+
 export function absoluteUrl() {
-  return (
-    process.env.BASE_URL ?? process.env.VERCEL_URL ?? "http://localhost:3000"
-  );
+  return process.env.BASE_URL ?? getVercelUrl() ?? "http://localhost:3000";
 }
