91106f508c0cb59210da509ca7b2f81049141a9d
Luke Vella <me@lukevella.com>
2022-06-01 11:00:00 +0100

    Revert changes to absolute-url

--- src/utils/absolute-url.ts
+++ src/utils/absolute-url.ts
@@ -1,7 +1,13 @@
-export function absoluteUrl() {
-  return process.env.NEXT_PUBLIC_BASE_URL ?? process.env.NEXT_PUBLIC_VERCEL_URL
+const getVercelUrl = () => {
+  return process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : typeof window !== "undefined"
-    ? window.origin
-    : "http://localhost:3000";
+    : null;
+};
+
+export function absoluteUrl() {
+  return (
+    process.env.NEXT_PUBLIC_BASE_URL ??
+    getVercelUrl() ??
+    "http://localhost:3000"
+  );
 }
