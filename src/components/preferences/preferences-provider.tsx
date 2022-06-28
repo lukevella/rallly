bbf32c902c3df95939ed8ef7159039dedae83b27
Luke Vella <me@lukevella.com>
2022-06-28 17:48:30 +0100

    Remove strict thresholds

--- src/components/preferences/preferences-provider.tsx
+++ src/components/preferences/preferences-provider.tsx
@@ -16,22 +16,7 @@ type TimeFormat = "12h" | "24h";
 type StartOfWeek = "monday" | "sunday";
 
 dayjs.extend(localizedFormat);
-dayjs.extend(relativeTime, {
-  rounding: (x) => x,
-  thresholds: [
-    { l: "s", r: 1 },
-    { l: "m", r: 1 },
-    { l: "mm", r: 59, d: "minute" },
-    { l: "h", r: 1 },
-    { l: "hh", r: 23, d: "hour" },
-    { l: "d", r: 1 },
-    { l: "dd", r: 29, d: "day" },
-    { l: "M", r: 1 },
-    { l: "MM", r: 11, d: "month" },
-    { l: "y" },
-    { l: "yy", d: "year" },
-  ],
-});
+dayjs.extend(relativeTime);
 dayjs.extend(localeData);
 dayjs.extend(isSameOrBefore);
 dayjs.extend(isBetween);
