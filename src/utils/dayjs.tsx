6e28d90b22e3010474adf6d0b1d5b47e5ff2fd40
Luke Vella <me@lukevella.com>
2022-07-28 09:02:30 +0100

    Fix test

--- src/utils/dayjs.tsx
+++ src/utils/dayjs.tsx
@@ -109,7 +109,7 @@ export const DayjsProvider: React.VoidFunctionComponent<{
     weekStart: weekStartsOn ? (weekStartsOn === "monday" ? 1 : 0) : undefined,
     formats: {
       ...dayjsLocale.formats,
-      LT: timeFormat === "12h" ? "h:mm a" : "H:mm",
+      LT: timeFormat === "12h" ? "h:mm A" : "H:mm",
     },
   });
 
