7f41abc74e08f6e7ee03fa103384101baa16b46b
Luke Vella <me@lukevella.com>
2022-08-18 13:59:17 +0100

    Revert "Turn off locale detection"
    This reverts commit d28d3648f1d46879fb34cd84a35f30a6c652bbf5.

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -6,6 +6,5 @@ module.exports = {
     locales: ["en", "es", "de", "fr", "it", "ko", "sv", "pt", "pt-BR"],
     localePath: path.resolve("./public/locales"),
     reloadOnPrerender: process.env.NODE_ENV === "development",
-    localeDetection: false,
   },
 };
