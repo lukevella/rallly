0280489e22993e53224488d15d6d46da0c899153
Luke Vella <me@lukevella.com>
2022-10-26 15:10:36 +0100

    Add czech locale (#339)

--- src/components/poll/language-selector.tsx
+++ src/components/poll/language-selector.tsx
@@ -19,19 +19,20 @@ export const LanguageSelect: React.VoidFunctionComponent<{
       }}
     >
       <option value="en">English</option>
+      <option value="cs">Česky</option>
+      <option value="zh">汉语</option>
       <option value="da">Dansk</option>
       <option value="de">Deutsch</option>
       <option value="es">Español</option>
       <option value="fr">Français</option>
-      <option value="hu">Magyar</option>
       <option value="it">Italiano</option>
       <option value="ko">한국어</option>
+      <option value="hu">Magyar</option>
       <option value="nl">Nederlands</option>
-      <option value="pt">Português</option>
       <option value="pl">Polski</option>
+      <option value="pt">Português</option>
       <option value="pt-BR">Português - Brasil</option>
       <option value="sv">Svenska</option>
-      <option value="zh">汉语</option>
     </select>
   );
 };
