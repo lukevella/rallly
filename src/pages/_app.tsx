0bc0005fd07e1b837e1727d3e467926158bbf68b
Luke Vella <me@lukevella.com>
2023-01-20 14:44:15 +0000

    Switch to hook for crisp chat

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import "~/style.css";
 
 import { NextPage } from "next";
 import { AppProps } from "next/app";
-import dynamic from "next/dynamic";
 import Head from "next/head";
 import { appWithTranslation } from "next-i18next";
 import PlausibleProvider from "next-plausible";
@@ -13,14 +12,13 @@ import { Toaster } from "react-hot-toast";
 
 import Maintenance from "@/components/maintenance";
 
+import { useCrispChat } from "../components/crisp-chat";
 import { absoluteUrl } from "../utils/absolute-url";
 import { trpcNext } from "../utils/trpc";
 
-const CrispChat = dynamic(() => import("@/components/crisp-chat"), {
-  ssr: false,
-});
-
 const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
+  useCrispChat();
+
   if (process.env.NEXT_PUBLIC_MAINTENANCE_MODE === "1") {
     return <Maintenance />;
   }
@@ -57,7 +55,6 @@ const MyApp: NextPage<AppProps> = ({ Component, pageProps }) => {
           content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
         />
       </Head>
-      <CrispChat />
       <Toaster />
       <Component {...pageProps} />
     </PlausibleProvider>
