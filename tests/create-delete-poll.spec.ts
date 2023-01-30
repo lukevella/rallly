e8969cc5e1a5ef9db238ec873d97b8b6c1e1b8ea
Luke Vella <me@lukevella.com>
2023-01-30 10:09:02 +0000

    Fix linting

--- tests/create-delete-poll.spec.ts
+++ tests/create-delete-poll.spec.ts
@@ -1,4 +1,4 @@
-import { expect, Page, test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 import smtpTester from "smtp-tester";
 
 test.describe.serial(() => {
@@ -6,7 +6,7 @@ test.describe.serial(() => {
 
   let pollUrl: string;
 
-  test.beforeAll(async ({ browser }) => {
+  test.beforeAll(async () => {
     mailServer = smtpTester.init(4025);
   });
 
