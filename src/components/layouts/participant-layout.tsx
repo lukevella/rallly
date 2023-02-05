3b1307724d12578501c4244b6f7f1d06dd52ecbe
Luke Vella <me@lukevella.com>
2023-02-05 11:31:06 +0000

    Remove powered by rallly badge

--- src/components/layouts/participant-layout.tsx
+++ src/components/layouts/participant-layout.tsx
@@ -1,35 +1,15 @@
-import { Trans, useTranslation } from "next-i18next";
-
 import { MobileNavigation } from "./standard-layout/mobile-navigation";
 
-const Logo = () => {
-  return <span className="font-bold tracking-wider">RALLLY</span>;
-};
 export const ParticipantLayout = ({
   children,
 }: {
   children?: React.ReactNode;
 }) => {
-  const { t } = useTranslation("app");
   return (
     <div className="bg-pattern min-h-full sm:space-y-8">
       <MobileNavigation />
       <div className="mx-auto max-w-3xl space-y-4 px-3 pb-8">
         <div>{children}</div>
-        <div className="flex justify-center text-center">
-          <a
-            href="https://rallly.co"
-            className="rounded-full bg-primary-500 px-4 py-2 text-sm text-white shadow-sm transition-colors hover:border-primary-500 hover:bg-primary-500/90 active:bg-primary-500"
-          >
-            <Trans
-              t={t}
-              i18nKey="poweredByRallly"
-              components={{
-                logo: <Logo />,
-              }}
-            />
-          </a>
-        </div>
       </div>
     </div>
   );
