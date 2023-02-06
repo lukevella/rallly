6fda4447b8086b61f04aa4b5a88707e32c6d342f
Luke Vella <me@lukevella.com>
2023-02-06 11:05:30 +0000

    Remove unused imports

--- src/components/create-poll.tsx
+++ src/components/create-poll.tsx
@@ -1,5 +1,4 @@
 import { NextPage } from "next";
-import Head from "next/head";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
 import posthog from "posthog-js";
--- src/components/poll.tsx
+++ src/components/poll.tsx
@@ -1,4 +1,3 @@
-import Head from "next/head";
 import { useTranslation } from "next-i18next";
 import React from "react";
 
