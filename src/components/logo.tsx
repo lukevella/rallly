e0f4ae1f45118b6e3838470bc4ed818dc725d42b
Luke Vella <me@lukevella.com>
2023-02-10 10:21:39 +0000

    🚩 Add badge when in beta environment

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,16 +3,23 @@ import clsx from "clsx";
 export const Logo = (props: { className?: string; color?: boolean }) => {
   const { color = true } = props;
   return (
-    <span
-      className={clsx(
-        "font-semibold uppercase tracking-widest",
-        {
-          "text-primary-500": color,
-        },
-        props.className,
-      )}
-    >
-      Rallly
+    <span className="inline-flex items-center">
+      <span
+        className={clsx(
+          "font-semibold uppercase tracking-widest",
+          {
+            "text-primary-500": color,
+          },
+          props.className,
+        )}
+      >
+        Rallly
+      </span>
+      {process.env.NEXT_PUBLIC_BETA === "1" ? (
+        <span className="ml-2 inline-block animate-pulse rounded bg-rose-500 px-1 text-xs lowercase tracking-tight text-white">
+          beta
+        </span>
+      ) : null}
     </span>
   );
 };
