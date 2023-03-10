0be84c7d8d48f563a4b5cc8bf19e32eeffc1786a
Luke Vella <me@lukevella.com>
2023-03-10 17:20:13 +0000

    Fix eslint

--- apps/web/src/components/layouts/standard-layout.tsx
+++ apps/web/src/components/layouts/standard-layout.tsx
@@ -1,4 +1,4 @@
-import { domMax, LazyMotion, m } from "framer-motion";
+import { domMax, LazyMotion } from "framer-motion";
 import React from "react";
 
 import { DayjsProvider } from "@/utils/dayjs";
