5995c8e7e40e42785be1016da2d805fe4820abbb
Luke Vella <me@lukevella.com>
2023-03-13 12:07:09 +0000

    Fix linting and unused styles

--- apps/web/src/components/poll/language-selector.tsx
+++ apps/web/src/components/poll/language-selector.tsx
@@ -22,29 +22,29 @@ export const LanguageSelect: React.FunctionComponent<{
           onChange?.(e.target.value);
         }}
       >
-      <option value="en">English</option>
-      <option value="ca">Català</option>
-      <option value="cs">Česky</option>
-      <option value="zh">汉语</option>
-      <option value="da">Dansk</option>
-      <option value="de">Deutsch</option>
-      <option value="es">Español</option>
-      <option value="fi">Suomi</option>
-      <option value="fr">Français</option>
-      <option value="hr">Hrvatski</option>
-      <option value="it">Italiano</option>
-      <option value="ko">한국어</option>
-      <option value="hu">Magyar</option>
-      <option value="nl">Nederlands</option>
-      <option value="pl">Polski</option>
-      <option value="pt">Português</option>
-      <option value="pt-BR">Português - Brasil</option>
-      <option value="ru">Pусский</option>
-      <option value="sk">Slovenčina</option>
-      <option value="sv">Svenska</option>
+        <option value="en">English</option>
+        <option value="ca">Català</option>
+        <option value="cs">Česky</option>
+        <option value="zh">汉语</option>
+        <option value="da">Dansk</option>
+        <option value="de">Deutsch</option>
+        <option value="es">Español</option>
+        <option value="fi">Suomi</option>
+        <option value="fr">Français</option>
+        <option value="hr">Hrvatski</option>
+        <option value="it">Italiano</option>
+        <option value="ko">한국어</option>
+        <option value="hu">Magyar</option>
+        <option value="nl">Nederlands</option>
+        <option value="pl">Polski</option>
+        <option value="pt">Português</option>
+        <option value="pt-BR">Português - Brasil</option>
+        <option value="ru">Pусский</option>
+        <option value="sk">Slovenčina</option>
+        <option value="sv">Svenska</option>
       </select>
-      <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none select-after">
-        <DropdownIcon className="group-hover:text-primary-500 w-5 opacity-75 group-hover:opacity-100"/>
+      <div className="select-after pointer-events-none absolute inset-y-0 right-0 flex items-center px-2">
+        <DropdownIcon className="w-5" />
       </div>
     </div>
   );
