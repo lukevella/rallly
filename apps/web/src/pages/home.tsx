207e8315f9cdd81d5b01d90eddfce1341c3194e8
Luke Vella <me@lukevella.com>
2023-03-14 16:05:00 +0000

    Hide landing page

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -1,13 +1,20 @@
 import { GetServerSideProps } from "next";
 
 import Home from "@/components/home";
+import { composeGetServerSideProps } from "@/utils/auth";
 import { withPageTranslations } from "@/utils/with-page-translations";
 
 export default function Page() {
   return <Home />;
 }
 
-export const getServerSideProps: GetServerSideProps = withPageTranslations([
-  "common",
-  "homepage",
-]);
+export const getServerSideProps: GetServerSideProps = composeGetServerSideProps(
+  async () => {
+    // TODO (Luke Vella) [2023-03-14]: Remove this once we split the app from the landing page
+    if (process.env.DISABLE_LANDING_PAGE === "true") {
+      return { notFound: true };
+    }
+    return { props: {} };
+  },
+  withPageTranslations(["common", "homepage"]),
+);
