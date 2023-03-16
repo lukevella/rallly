53fbdc9c15a4809156a35acd96c83ad4d73c1fda
Luke Vella <me@lukevella.com>
2023-03-16 10:10:47 +0000

    🐛 Fix `DISABLE_LANDING_PAGE` not working

--- apps/web/src/pages/home.tsx
+++ apps/web/src/pages/home.tsx
@@ -12,7 +12,7 @@ export const getServerSideProps: GetServerSideProps = composeGetServerSideProps(
   async () => {
     // TODO (Luke Vella) [2023-03-14]: Remove this once we split the app from the landing page
     if (process.env.DISABLE_LANDING_PAGE === "true") {
-      return { notFound: true };
+      return { redirect: { destination: "/new", permanent: false } };
     }
     return { props: {} };
   },
