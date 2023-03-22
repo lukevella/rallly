e6ee385705f4295fa54c0fca859237eb36a76b87
jonas-hoebenreich <jonas.hoebenreich@tum.de>
2023-03-23 00:07:01 +0100

    make user-scalable

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -91,7 +91,7 @@ const MyApp: NextPage<AppPropsWithLayout> = ({ Component, pageProps }) => {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no"
+          content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, user-scalable=yes"
         />
       </Head>
       <Toaster />
