0aad039244bcff533ae36687d6e6531df0740886
Mark Pitblado <86988982+arcticFox-git@users.noreply.github.com>
2023-03-22 23:42:42 -0700

    Adding version number to footer.

--- apps/web/src/components/layouts/page-layout/footer.tsx
+++ apps/web/src/components/layouts/page-layout/footer.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { Trans, useTranslation } from "next-i18next";
+import { useEffect, useState } from "react";
 import * as React from "react";
+import process from "process";
 
 import Discord from "@/components/icons/discord.svg";
 import Star from "@/components/icons/star.svg";
@@ -17,12 +19,21 @@ import { LanguageSelect } from "../../poll/language-selector";
 const Footer: React.FunctionComponent = () => {
   const { t } = useTranslation("common");
   const router = useRouter();
+  const [appVersion, setAppVersion] = useState("");
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_APP_VERSION !== undefined) {
+      setAppVersion('v' + process.env.NEXT_PUBLIC_APP_VERSION || '');
+    }
+  }, []);
+
   return (
     <div className="mt-16 bg-gradient-to-b from-gray-50/0 via-gray-50 to-gray-50 ">
       <div className="mx-auto max-w-7xl space-y-8 p-8 lg:flex lg:space-x-16 lg:space-y-0">
         <div className=" lg:w-2/6">
           <Logo className="w-32 text-slate-400" />
           <div className="mb-8 mt-4 text-slate-400">
+            <p>{appVersion}</p>
             <p>
               <Trans
                 t={t}
