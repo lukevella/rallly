4b5c8dd16c2b8829c46dc510c69c0231b7ec7709
Luke Vella <me@lukevella.com>
2023-03-27 12:15:15 +0100

    🔥 Drop authorName from polls table

---
+++ packages/database/prisma/migrations/20230327105647_remove_author_name/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `author_name` on the `polls` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "polls" DROP COLUMN "author_name";
