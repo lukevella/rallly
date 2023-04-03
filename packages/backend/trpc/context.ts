7da51bfeccb84cd995f888b47802e10f9492cebf
Luke Vella <me@lukevella.com>
2023-04-03 18:35:43 +0100

    Clean up

--- packages/backend/trpc/context.ts
+++ packages/backend/trpc/context.ts
@@ -1,29 +1,23 @@
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
-import { IronSession } from "iron-session";
 import { GetServerSidePropsContext } from "next";
 import superjson from "superjson";
 
 import { randomid } from "../utils/nanoid";
 import { appRouter } from "./routers";
 
-export async function createUser(session: IronSession) {
-  session.user = {
-    id: `user-${randomid()}`,
-    isGuest: true,
-  };
-  await session.save();
-
-  return session.user;
-}
-
 export async function createContext(
   opts: trpcNext.CreateNextContextOptions | GetServerSidePropsContext,
 ) {
   let user = opts.req.session.user;
   if (!user) {
-    user = await createUser(opts.req.session);
+    user = {
+      id: `user-${randomid()}`,
+      isGuest: true,
+    };
+    opts.req.session.user = user;
+    opts.req.session.save();
   }
   return { user, session: opts.req.session };
 }
