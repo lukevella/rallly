3bf837595a8487144fd2203b92a5defa08872477
Luke Vella <me@lukevella.com>
2023-04-13 22:35:05 +0100

    🐛 Remove version suffix in layout
    The docker image already has the suffix since this comes from the git tag

--- apps/web/src/components/layouts/standard-layout.tsx
+++ apps/web/src/components/layouts/standard-layout.tsx
@@ -14,7 +14,7 @@ import { MobileNavigation } from "./standard-layout/mobile-navigation";
 const Feedback = dynamic(() => import("../feedback"), { ssr: false });
 
 const appVersion = process.env.NEXT_PUBLIC_APP_VERSION
-  ? `v${process.env.NEXT_PUBLIC_APP_VERSION}`
+  ? process.env.NEXT_PUBLIC_APP_VERSION
   : null;
 
 const AppVersion = () => {
--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 export NEXT_PUBLIC_APP_VERSION=$(node -p "require('./package.json').version")
-echo "Set NEXT_PUBLIC_APP_VERSION=$NEXT_PUBLIC_APP_VERSION"
+echo "Set NEXT_PUBLIC_APP_VERSION=v$NEXT_PUBLIC_APP_VERSION"
 yarn build
 # Deploy migration using direct database connection (no connection pool)
 DATABASE_URL=$DIRECT_DATABASE_URL yarn db:deploy
