ffaaedf31317e257aa4f394fd21232b3606b7c72
Luke Vella <me@lukevella.com>
2023-04-25 10:08:51 +0100

    Remove unused i18n keys

--- apps/web/i18next-scanner.config.js
+++ apps/web/i18next-scanner.config.js
@@ -1,16 +1,27 @@
+const typescriptTransform = require("i18next-scanner-typescript");
+
 module.exports = {
   input: ["src/**/*.{ts,tsx}"],
-  removeUnusedKeys: true,
   options: {
     keySeparator: ".",
     nsSeparator: false,
     defaultNs: "app",
-    defaultValue: "__STRING_NOT_TRANSLATED__",
+    defaultValue: function (lng) {
+      if (lng === "en") {
+        return "__STRING_NOT_TRANSLATED__";
+      }
+      return "";
+    },
     lngs: ["en"],
     ns: ["app"],
+    plural: false,
+    removeUnusedKeys: true,
     func: {
       list: ["t"],
-      extensions: [".ts", ".tsx"],
+      extensions: [".js", ".jsx"],
+    },
+    trans: {
+      extensions: [".js", ".jsx"],
     },
     resource: {
       loadPath: "public/locales/{{lng}}/{{ns}}.json",
@@ -19,4 +30,5 @@ module.exports = {
   },
   format: "json",
   fallbackLng: "en",
+  transform: typescriptTransform(),
 };
