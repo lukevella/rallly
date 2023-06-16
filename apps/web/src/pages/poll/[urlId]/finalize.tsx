c962b7e06c7255b0d5ae55888adfc2c394340de2
Luke Vella <me@lukevella.com>
2023-06-16 08:41:08 +0100

    Add more info on finalize page

--- apps/web/src/pages/poll/[urlId]/finalize.tsx
+++ apps/web/src/pages/poll/[urlId]/finalize.tsx
@@ -1,3 +1,9 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@rallly/ui/accordion";
 import { Button } from "@rallly/ui/button";
 import {
   Card,
@@ -42,7 +48,7 @@ const Page: NextPageWithLayout = () => {
           </div>
           <h1 className="mb-1">{`Finalize your Poll`}</h1>
           <p className="text-muted-foreground mx-auto max-w-lg">
-            {`We're launching a paid tier soon with more features. Sign up to be notified when it's ready.`}
+            {`We're launching a paid tier soon with more features!`}
           </p>
         </div>
         <Card className="pointer-events-none mx-auto -mb-32 max-w-2xl select-none">
@@ -85,6 +91,40 @@ const Page: NextPageWithLayout = () => {
           </CardFooter>
         </Card>
       </div>
+      <Accordion type="single">
+        <AccordionItem value="whyNotFree">
+          <AccordionTrigger>{`Why isn't this free?`}</AccordionTrigger>
+          <AccordionContent>
+            {`Rallly has been running for over 7 years and has been free for everyone to use. We've been able to do this thanks to the generosity of our users who have donated to help keep the service running. However, we've reached a point where we need to start generating revenue to be able to grow and continue to develop new features. We've decided to introduce a paid tier to help us achieve this.`}
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="whatAreTheBenefits">
+          <AccordionTrigger>
+            {`What are the benefits of upgrading to Rallly Pro?`}
+          </AccordionTrigger>
+          <AccordionContent>
+            {`As a Pro user, you will be able to finalize your polls and send calendar invites to your participants. We plan to deliver many more exclusive features as well and as a Pro user you will have access to all of these features as well. You will also receive priority support and be able to help shape the future of Rallly.`}
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="3">
+          <AccordionTrigger>How much does Rallly Pro cost?</AccordionTrigger>
+          <AccordionContent>
+            {`For early adopters, Rallly Pro will be available for just $4.99 per month or $24.99 if you sign up for a whole year. We will adjust the price as we add more features so you will be getting a significantly reduced rate by signing up early.`}
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="4">
+          <AccordionTrigger>Can I still use Rallly for free?</AccordionTrigger>
+          <AccordionContent>
+            {`Yes, the free service remains exactly the same.`}
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="5">
+          <AccordionTrigger>How can I upgrade to Rallly Pro?</AccordionTrigger>
+          <AccordionContent>
+            {`We're just setting up a checkout system and once this is ready you will be able to upgrade.`}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </div>
   );
 };
