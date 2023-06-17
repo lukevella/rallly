35412623407884d1ee921f114aa945d6f3cbf020
Luke Vella <me@lukevella.com>
2023-06-17 12:57:41 +0100

    Add mailserver

--- apps/web/tests/edit-options.spec.ts
+++ apps/web/tests/edit-options.spec.ts
@@ -1,18 +1,25 @@
 import { expect, Page, test } from "@playwright/test";
+import smtpTester, { SmtpTester } from "smtp-tester";
 import { EditOptionsPage } from "tests/edit-options-page";
 import { NewPollPage } from "tests/new-poll-page";
 
 test.describe("edit options", () => {
   let page: Page;
   let editOptionsPage: EditOptionsPage;
+  let mailServer: SmtpTester;
 
   test.beforeAll(async ({ browser }) => {
     page = await browser.newPage();
+    mailServer = smtpTester.init(4025);
     const newPollPage = new NewPollPage(page);
     await newPollPage.goto();
     const pollPage = await newPollPage.createPollAndCloseDialog();
     await pollPage.addParticipant("Mark");
     editOptionsPage = await pollPage.editOptions();
+  });
+
+  test.afterAll(async () => {
+    mailServer.stop();
   });
 
   test("should show warning when deleting options with votes in them", async () => {
