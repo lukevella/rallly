47db612dc7629fc84b3aaf5452d8fc27ab99b859
Luke Vella <me@lukevella.com>
2023-07-13 14:28:07 +0100

    Fix test

--- apps/web/tests/i18n.spec.ts
+++ apps/web/tests/i18n.spec.ts
@@ -3,7 +3,7 @@ import test, { expect } from "@playwright/test";
 test("should show correct language if supported", async ({ browser }) => {
   const context = await browser.newContext({ locale: "de" });
   const page = await context.newPage();
-  await page.goto("/");
+  await page.goto("/new");
   await expect(page.locator("text=Neue Umfrage")).toBeVisible();
 });
 
