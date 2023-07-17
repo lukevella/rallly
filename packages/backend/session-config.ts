68eb1320d901ccdf106867be277667cd3ceb5e97
Luke Vella <me@lukevella.com>
2023-07-17 17:18:22 +0100

    🍪 Add domain to session cookie

--- packages/backend/session-config.ts
+++ packages/backend/session-config.ts
@@ -3,6 +3,7 @@ export const sessionConfig = {
   cookieName: "rallly-session",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
+    domain: process.env.SESSION_COOKIE_DOMAIN,
   },
   ttl: 60 * 60 * 24 * 30, // 30 days
 };
