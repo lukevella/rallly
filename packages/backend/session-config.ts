e16ce8785b91c2b273e17f0f7822d423340f9255
Luke Vella <me@lukevella.com>
2023-07-17 18:20:04 +0100

    Revert "🍪 Add domain to session cookie (#765)"
    This reverts commit 384201365aebeeee543f756986f3ab2c5aec98a8.

--- packages/backend/session-config.ts
+++ packages/backend/session-config.ts
@@ -3,7 +3,6 @@ export const sessionConfig = {
   cookieName: "rallly-session",
   cookieOptions: {
     secure: process.env.NODE_ENV === "production",
-    domain: process.env.SESSION_COOKIE_DOMAIN,
   },
   ttl: 60 * 60 * 24 * 30, // 30 days
 };
