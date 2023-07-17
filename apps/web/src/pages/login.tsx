8521fdd2fadc5d07b00c376bbe399c90b02d3158
Luke Vella <me@lukevella.com>
2023-07-17 20:43:03 +0100

    remove unused

--- apps/web/src/pages/login.tsx
+++ apps/web/src/pages/login.tsx
@@ -1,5 +1,4 @@
 import { Loader2Icon } from "@rallly/icons";
-import { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useTranslation } from "next-i18next";
