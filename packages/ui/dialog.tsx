1caf0fc07e9a84f01811d3d253371bb1b73312b9
Luke Vella <me@lukevella.com>
2023-07-20 12:21:14 +0100

    Allow hiding close button in dialog

--- packages/ui/dialog.tsx
+++ packages/ui/dialog.tsx
@@ -42,8 +42,9 @@ const DialogContent = React.forwardRef<
   React.ElementRef<typeof DialogPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DialogPrimitive.Content> & {
     size?: "sm" | "md" | "lg";
+    hideCloseButton?: boolean;
   }
->(({ className, children, size = "md", ...props }, ref) => (
+>(({ className, children, size = "md", hideCloseButton, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
@@ -61,8 +62,12 @@ const DialogContent = React.forwardRef<
     >
       {children}
       <DialogPrimitive.Close className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none">
-        <XIcon className="h-4 w-4" />
-        <span className="sr-only">Close</span>
+        {!hideCloseButton ? (
+          <>
+            <XIcon className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </>
+        ) : null}
       </DialogPrimitive.Close>
     </DialogPrimitive.Content>
   </DialogPortal>
