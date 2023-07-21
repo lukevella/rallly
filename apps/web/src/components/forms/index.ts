5ecd75ca0cf0126c2eec52f5b9f9e6d7a89a35af
Luke Vella <me@lukevella.com>
2023-07-21 18:41:28 +0100

    Delete unused

--- apps/web/src/components/forms/index.ts
+++ apps/web/src/components/forms/index.ts
@@ -3,5 +3,3 @@ export { PollDetailsForm } from "./poll-details-form";
 export type { PollOptionsData } from "./poll-options-form/poll-options-form";
 export { default as PollOptionsForm } from "./poll-options-form/poll-options-form";
 export * from "./types";
-export type { UserDetailsData } from "./user-details-form";
-export { UserDetailsForm } from "./user-details-form";
