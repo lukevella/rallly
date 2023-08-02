04b4724fd6d12749f15e6d51dc9ebe4b3c016293
Luke Vella <me@lukevella.com>
2023-08-02 18:10:12 +0100

    Fix button interaction

--- apps/web/src/components/featurebase.tsx
+++ apps/web/src/components/featurebase.tsx
@@ -1,5 +1,6 @@
 import { trpc } from "@rallly/backend";
 import { HelpCircleIcon } from "@rallly/icons";
+import { cn } from "@rallly/ui";
 import { Button } from "@rallly/ui/button";
 import Script from "next/script";
 import React from "react";
@@ -15,40 +16,39 @@ export const Changelog = ({ className }: { className?: string }) => {
     const win = window as any;
 
     if (typeof win.Featurebase !== "function") {
-      win.Featurebase = function () {
-        // eslint-disable-next-line prefer-rest-params
-        (win.Featurebase.q = win.Featurebase.q || []).push(arguments);
+      win.Featurebase = function (...args: unknown[]) {
+        (win.Featurebase.q = win.Featurebase.q || []).push(args);
       };
     }
-    win.Featurebase(
-      "initialize_changelog_widget",
-      {
-        organization: "rallly", // Replace this with your featurebase organization name
-        placement: "bottom", // Choose between right, left, top, bottom placement
-        theme: "light", // Choose between dark or light theme
-      },
-      console.error,
-    );
+
+    win.Featurebase("initialize_changelog_widget", {
+      organization: "rallly", // Replace this with your featurebase organization name
+      placement: "bottom", // Choose between right, left, top, bottom placement
+      theme: "light", // Choose between dark or light theme
+    });
   }, []);
 
   if (!isFeaturebaseEnabled) return null;
 
   return (
-    <Button
-      className={className}
-      size="sm"
-      variant="ghost"
-      data-featurebase-changelog
-    >
-      <HelpCircleIcon className="h-4 w-4" />
-      <span className="hidden sm:block">
-        <Trans key="whatsNew" defaults="What's new?" />
-      </span>
-      <span
-        id="fb-update-badge"
-        className="bg-primary rounded-md px-1 py-px text-xs text-gray-100 empty:hidden"
-      />
-    </Button>
+    <>
+      <Script src="https://do.featurebase.app/js/sdk.js" id="featurebase-sdk" />
+      <Button
+        className={cn("[&>*]:pointer-events-none", className)}
+        size="sm"
+        variant="ghost"
+        data-featurebase-changelog
+      >
+        <HelpCircleIcon className="h-4 w-4" />
+        <span className="hidden sm:block">
+          <Trans key="whatsNew" defaults="What's new?" />
+        </span>
+        <span
+          id="fb-update-badge"
+          className="bg-primary rounded-md px-1 py-px text-xs text-gray-100 empty:hidden"
+        />
+      </Button>
+    </>
   );
 };
 
@@ -56,18 +56,16 @@ export const FeaturebaseProvider = ({ children }: React.PropsWithChildren) => {
   const { data: user } = trpc.whoami.get.useQuery();
 
   React.useEffect(() => {
-    if (user?.isGuest !== false) return;
+    if (user?.isGuest !== false || !isFeedbackEnabled) return;
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const win = window as any;
-
     if (typeof win.Featurebase !== "function") {
       win.Featurebase = function () {
         // eslint-disable-next-line prefer-rest-params
         (win.Featurebase.q = win.Featurebase.q || []).push(arguments);
       };
     }
-
-    if (!isFeedbackEnabled) return;
 
     win.Featurebase(
       "identify",
@@ -87,6 +85,7 @@ export const FeaturebaseProvider = ({ children }: React.PropsWithChildren) => {
       },
     );
   }, [user]);
+
   return (
     <FeaturebaseContext.Provider value={true}>
       <Script src="https://do.featurebase.app/js/sdk.js" id="featurebase-sdk" />
