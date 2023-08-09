b9ca609236bf72377f4a7b455940fc591495eaa8
Luke Vella <me@lukevella.com>
2023-08-09 16:17:53 +0100

    🔗 Add link to app on auth pages if auth isn’t required

--- apps/web/src/components/auth/auth-layout.tsx
+++ apps/web/src/components/auth/auth-layout.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
+import { Trans } from "next-i18next";
 import React from "react";
 
 export const AuthLayout = ({ children }: { children?: React.ReactNode }) => {
   return (
     <div className="h-full p-3 sm:p-8">
-      <div className="flex h-full items-start justify-center">
+      <div className="mx-auto w-fit">
         <div className="w-[480px] max-w-full overflow-hidden rounded-lg border bg-white shadow-sm">
           <div className="bg-pattern border-t-primary-600 border-b border-t-4 bg-gray-500/5 p-4 text-center sm:p-8">
             <Image
@@ -17,6 +19,16 @@ export const AuthLayout = ({ children }: { children?: React.ReactNode }) => {
           </div>
           <div className="p-4 sm:p-6">{children}</div>
         </div>
+        {process.env.AUTH_REQUIRED === "true" ? null : (
+          <p className="mt-8 text-center">
+            <Link
+              href="/polls"
+              className="text-muted-foreground hover:underline"
+            >
+              <Trans i18nKey="continueAsGuest" defaults="Continue as Guest" />
+            </Link>
+          </p>
+        )}
       </div>
     </div>
   );
