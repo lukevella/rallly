202b0836b793d8bc10590da5229fc53a35682a3b
Luke Vella <me@lukevella.com>
2023-08-25 15:57:08 +0100

    Add conditional wrappers for subscription

--- apps/web/src/contexts/plan.tsx
+++ apps/web/src/contexts/plan.tsx
@@ -1,4 +1,5 @@
 import { trpc } from "@rallly/backend";
+import React from "react";
 
 export const usePlan = () => {
   const { data } = trpc.user.subscription.useQuery();
@@ -6,4 +7,16 @@ export const usePlan = () => {
   const isPaid = data?.active === true;
 
   return isPaid ? "paid" : "free";
+};
+
+export const IfSubscribed = ({ children }: React.PropsWithChildren) => {
+  const plan = usePlan();
+
+  return plan === "paid" ? <>{children}</> : null;
+};
+
+export const IfFreeUser = ({ children }: React.PropsWithChildren) => {
+  const plan = usePlan();
+
+  return plan === "free" ? <>{children}</> : null;
 };
