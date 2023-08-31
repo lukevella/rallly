248a6d71ab1314057f690f725db8443d20ddf883
Luke Vella <me@lukevella.com>
2023-08-31 18:05:51 +0100

    🐛 Fix unintentional overflow in auth layout

--- apps/web/src/components/auth/auth-layout.tsx
+++ apps/web/src/components/auth/auth-layout.tsx
@@ -6,8 +6,8 @@ import React from "react";
 export const AuthLayout = ({ children }: { children?: React.ReactNode }) => {
   return (
     <div className="h-full p-3 sm:p-8">
-      <div className="mx-auto w-fit">
-        <div className="w-[480px] max-w-full overflow-hidden rounded-lg border bg-white shadow-sm">
+      <div className="mx-auto max-w-lg">
+        <div className="overflow-hidden rounded-lg border bg-white shadow-sm">
           <div className="bg-pattern border-t-primary-600 border-b border-t-4 bg-gray-500/5 p-4 text-center sm:p-8">
             <Image
               src="/static/logo.svg"
