9f4b84cd34fd7ed1f6951c54cad3318f24d8f926
Luke Vella <me@lukevella.com>
2023-09-06 16:46:07 +0100

    Fixes

--- packages/backend/session-config.ts
+++ packages/backend/session-config.ts
@@ -2,7 +2,7 @@ export const sessionConfig = {
   password: process.env.SECRET_PASSWORD ?? "",
   cookieName: "rallly-session",
   cookieOptions: {
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NEXT_PUBLIC_BASE_URL?.startsWith("https://") ?? false,
   },
   ttl: 60 * 60 * 24 * 30, // 30 days
 };
