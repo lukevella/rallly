e1002713d3db2a6e673ed07787cc85c57d7c4b00
Luke Vella <me@lukevella.com>
2023-09-15 12:57:49 +0100

    ♻️ Improve email abstraction

---
+++ packages/backend/utils/emailClient.ts
@@ -0,0 +1,16 @@
+import { EmailClient, SupportedEmailProviders } from "@rallly/emails";
+
+export const emailClient = new EmailClient({
+  openPreviews: process.env.NODE_ENV === "development",
+  provider: {
+    name: process.env.EMAIL_PROVIDER as SupportedEmailProviders,
+  },
+  mail: {
+    from: {
+      name: "Rallly",
+      address:
+        (process.env.NOREPLY_EMAIL as string) ||
+        (process.env.SUPPORT_EMAIL as string),
+    },
+  },
+});
