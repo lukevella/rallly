97fb600218756211f0d2605a41f23e00d5f58a6a
Luke Vella <me@lukevella.com>
2023-10-10 19:09:32 +0100

    Enable smoother animation when going to log page

--- apps/web/src/components/login-link.tsx
+++ apps/web/src/components/login-link.tsx
@@ -1,11 +1,12 @@
 import Link, { LinkProps } from "next/link";
-import { signIn } from "next-auth/react";
+import { useRouter } from "next/router";
 import React from "react";
 
 export const LoginLink = React.forwardRef<
   HTMLAnchorElement,
   React.PropsWithChildren<Omit<LinkProps, "href"> & { className?: string }>
 >(function LoginLink({ children, ...props }, ref) {
+  const router = useRouter();
   return (
     <Link
       ref={ref}
@@ -13,8 +14,8 @@ export const LoginLink = React.forwardRef<
       href="/login"
       onClick={async (e) => {
         e.preventDefault();
-        signIn();
         props.onClick?.(e);
+        router.push("/login?callbackUrl=" + encodeURIComponent(router.asPath));
       }}
     >
       {children}
