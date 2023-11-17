3decadfd2bda410d91fe0d6cac727ad8003c7a66
Luke Vella <me@lukevella.com>
2023-11-17 17:10:36 +0700

    ♻️ Require auth for self hosters with app router

--- apps/web/src/app/[locale]/(admin)/layout.tsx
+++ apps/web/src/app/[locale]/(admin)/layout.tsx
@@ -1,6 +1,34 @@
 "use client";
+import { signIn, useSession } from "next-auth/react";
+import React from "react";
+
 import { StandardLayout } from "@/components/layouts/standard-layout";
+import { isSelfHosted } from "@/utils/constants";
+
+const Auth = ({ children }: { children: React.ReactNode }) => {
+  const session = useSession();
+  const isAuthenticated = !!session.data?.user.email;
+
+  React.useEffect(() => {
+    if (!isAuthenticated) {
+      signIn();
+    }
+  }, [isAuthenticated]);
+
+  if (isAuthenticated) {
+    return <>{children}</>;
+  }
+
+  return null;
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  if (isSelfHosted) {
+    return (
+      <Auth>
+        <StandardLayout>{children}</StandardLayout>
+      </Auth>
+    );
+  }
   return <StandardLayout>{children}</StandardLayout>;
 }
