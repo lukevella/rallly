f7c8e8269b4434340ff34c914e1ed3c75b881b9d
Luke Vella <me@lukevella.com>
2023-11-26 11:33:43 +0700

    🧹 Remove unused env vars

--- apps/landing/declarations/environment.d.ts
+++ apps/landing/declarations/environment.d.ts
@@ -85,33 +85,6 @@ declare global {
        */
       OIDC_CLIENT_SECRET?: string;
       /**
-       * Scopes that should be used when configuring the oidc provider
-       */
-      OIDC_SCOPES?: string;
-      /**
-       * If Rallly should expect the oidc provider to return an ID token
-       */
-      OIDC_ID_TOKEN_EXPECTED?: string;
-      /**
-       * When using an oidc provider that support the userinfo endpoint, set this to "true" to
-       * use it instead of getting the user info from the ID token
-       */
-      OIDC_FORCE_USER_INFO?: string;
-      /**
-       * When using a provider that does not provide a userinfo endpoint in its discovery document,
-       * `OIDC_FORCE_USER_INFO` may be set to the URL of the userinfo endpoint.
-       * `OIDC_USER_INFO_URL` may not be usable when `OIDC_FORCE_USER_INFO` is set to `true`
-       */
-      OIDC_USER_INFO_URL?: string;
-      /**
-       * The name of the `name` field returned by the oidc provider
-       */
-      OIDC_NAME_CLAIM?: string;
-      /**
-       * The name of the `email` field returned by the oidc provider
-       */
-      OIDC_EMAIL_CLAIM?: string;
-      /**
        * AWS access key ID
        */
       AWS_ACCESS_KEY_ID?: string;
