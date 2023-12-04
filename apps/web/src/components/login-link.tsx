23a84096466644d182e7325b7d6e3c7a78c4f424
Luke Vella <me@lukevella.com>
2023-12-04 11:38:07 +0700

    Change login link behavior

--- apps/web/src/components/login-link.tsx
+++ apps/web/src/components/login-link.tsx
@@ -1,23 +1,17 @@
 import Link, { LinkProps } from "next/link";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export const LoginLink = React.forwardRef<
   HTMLAnchorElement,
   React.PropsWithChildren<Omit<LinkProps, "href"> & { className?: string }>
 >(function LoginLink({ children, ...props }, ref) {
-  const router = useRouter();
   const pathname = usePathname() ?? "/";
   return (
     <Link
       ref={ref}
       {...props}
-      href="/login"
-      onClick={async (e) => {
-        e.preventDefault();
-        props.onClick?.(e);
-        router.push("/login?callbackUrl=" + encodeURIComponent(pathname));
-      }}
+      href={`/login?callbackUrl=${encodeURIComponent(pathname)}`}
     >
       {children}
     </Link>
