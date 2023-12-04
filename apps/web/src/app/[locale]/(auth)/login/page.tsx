049b6e90f9310359235c0c6c4b3cfc026c34c0f4
Luke Vella <me@lukevella.com>
2023-12-04 11:23:15 +0700

    💄 Add loading ui for auth pages

--- apps/web/src/app/[locale]/(auth)/login/page.tsx
+++ apps/web/src/app/[locale]/(auth)/login/page.tsx
@@ -5,11 +5,11 @@ import { LoginForm } from "@/app/[locale]/(auth)/login/login-form";
 import { Params } from "@/app/[locale]/types";
 import { getTranslation } from "@/app/i18n";
 import { AuthCard } from "@/components/auth/auth-layout";
-import { isOIDCEnabled, isSelfHosted, oidcName } from "@/utils/constants";
+import { isOIDCEnabled, oidcName } from "@/utils/constants";
 
 // Self-hosted instances only have env vars for OIDC at runtime, so we need to
-// use force-dynamic to avoid statically rendering this page during build time..
-export const dynamic = isSelfHosted ? "force-dynamic" : "auto";
+// use force-dynamic to avoid statically rendering this page during build time.
+export const dynamic = "force-dynamic";
 
 export default async function LoginPage({ params }: { params: Params }) {
   const { t } = await getTranslation(params.locale);
