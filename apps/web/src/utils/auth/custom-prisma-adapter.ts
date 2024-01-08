5898755bbd0472bb1d8eccfa237d9f83f2df097b
Luke Vella <me@lukevella.com>
2024-01-08 09:04:25 +0700

    🔨 Patch OAuth providers returning unexpected fields (#972)

---
+++ apps/web/src/utils/auth/custom-prisma-adapter.ts
@@ -0,0 +1,37 @@
+/**
+ * This is a modified version of the default Prisma adapter that
+ * ignores fields returned by the OAuth provider that are not
+ * defined in the database schema when creating an account.
+ *
+ * This resolves issues where some OAuth providers return unexpected
+ * fields in their response which cause the Prisma adapter to throw
+ * an error.
+ *
+ * See: https://github.com/lukevella/rallly/issues/949
+ */
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@rallly/database";
+import { Adapter, AdapterAccount } from "next-auth/adapters";
+
+export function CustomPrismaAdapter(client: PrismaClient): Adapter {
+  return {
+    ...PrismaAdapter(client),
+    linkAccount: (data) => {
+      return client.account.create({
+        data: {
+          userId: data.userId,
+          type: data.type,
+          provider: data.provider,
+          providerAccountId: data.providerAccountId,
+          access_token: data.access_token as string,
+          expires_at: data.expires_at as number,
+          id_token: data.id_token as string,
+          token_type: data.token_type as string,
+          refresh_token: data.refresh_token as string,
+          scope: data.scope as string,
+          session_state: data.session_state as string,
+        },
+      }) as unknown as AdapterAccount;
+    },
+  };
+}
