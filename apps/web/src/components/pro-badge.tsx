b00fbb19af253d9f9a90322b2036cb16f4c023df
Luke Vella <me@lukevella.com>
2024-01-12 12:54:15 +0700

    Hide pro badge if already pro

--- apps/web/src/components/pro-badge.tsx
+++ apps/web/src/components/pro-badge.tsx
@@ -1,10 +1,15 @@
+"use client";
 import { Badge } from "@rallly/ui/badge";
 import { Trans } from "next-i18next";
 
+import { IfFreeUser } from "@/contexts/plan";
+
 export const ProBadge = ({ className }: { className?: string }) => {
   return (
-    <Badge className={className}>
-      <Trans i18nKey="planPro" />
-    </Badge>
+    <IfFreeUser>
+      <Badge className={className}>
+        <Trans i18nKey="planPro" />
+      </Badge>
+    </IfFreeUser>
   );
 };
