09f4b16f365b5eaca5510750840c426ca91e2ef5
Luke Vella <me@lukevella.com>
2024-01-12 22:25:05 +0700

    Fix next config

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -69,12 +69,9 @@ const sentryWebpackPluginOptions = {
   // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
-const withBundleAnalyzerConfig = withBundleAnalyzer(
-  nextConfig,
-  sentryWebpackPluginOptions,
-);
+const withBundleAnalyzerConfig = withBundleAnalyzer(nextConfig);
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = process.env.SENTRY_AUTH_TOKEN
-  ? withSentryConfig(withBundleAnalyzerConfig)
+  ? withSentryConfig(withBundleAnalyzerConfig, sentryWebpackPluginOptions)
   : withBundleAnalyzerConfig;
