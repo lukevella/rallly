44a0d953551c6908af2d593c0ed3070c91e39fc4
Luke Vella <me@lukevella.com>
2024-02-24 10:44:53 +0800

    🧹 Remove legacy user preferences (#1035)

--- packages/backend/trpc/routers/index.ts
+++ packages/backend/trpc/routers/index.ts
@@ -2,14 +2,12 @@ import { mergeRouters, router } from "../trpc";
 import { auth } from "./auth";
 import { polls } from "./polls";
 import { user } from "./user";
-import { userPreferences } from "./user-preferences";
 
 export const appRouter = mergeRouters(
   router({
     auth,
     polls,
     user,
-    userPreferences,
   }),
 );
 
