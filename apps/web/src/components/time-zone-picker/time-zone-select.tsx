2d58be9791613223f8a3d64cee9bb251d3e86684
Luke Vella <me@lukevella.com>
2024-02-24 09:02:38 +0800

    Clean up

--- apps/web/src/components/time-zone-picker/time-zone-select.tsx
+++ apps/web/src/components/time-zone-picker/time-zone-select.tsx
@@ -17,20 +17,7 @@ import { useTranslation } from "next-i18next";
 import React from "react";
 
 import { Trans } from "@/components/trans";
-import { parseIanaTimezone } from "@/utils/date-time-utils";
-import { supportedTimeZones } from "@/utils/supported-time-zones";
-
-export const groupedTimeZones = supportedTimeZones.reduce(
-  (acc, tz) => {
-    const { region, city } = parseIanaTimezone(tz);
-    if (!acc[region]) {
-      acc[region] = [];
-    }
-    acc[region].push({ timezone: tz, city });
-    return acc;
-  },
-  {} as Record<string, { timezone: string; city: string }[]>,
-);
+import { groupedTimeZones } from "@/utils/grouped-time-zone";
 
 interface TimeZoneCommandProps {
   value?: string;
