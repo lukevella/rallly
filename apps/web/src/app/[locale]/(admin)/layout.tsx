e5465beb342c8ffdc839b10a0b861706ed3e7105
Luke Vella <me@lukevella.com>
2024-05-02 11:58:57 +0800

    Update layout

--- apps/web/src/app/[locale]/(admin)/layout.tsx
+++ apps/web/src/app/[locale]/(admin)/layout.tsx
@@ -10,28 +10,22 @@ export default async function Layout({
 }: {
   children: React.ReactNode;
 }) {
-  function SidebarLayout() {
-    return (
-      <div className="bg-gray-100">
-        <MobileNavigation />
-        <div
-          className={cn(
-            "inset-y-0 z-50 hidden shrink-0 flex-col gap-y-5 overflow-y-auto px-5 py-4 lg:fixed lg:flex lg:w-72 lg:px-6 lg:py-4",
-          )}
-        >
-          <div>
-            <LogoLink />
-          </div>
-          <Sidebar />
+  return (
+    <div className="bg-gray-100">
+      <MobileNavigation />
+      <div
+        className={cn(
+          "inset-y-0 z-50 hidden shrink-0 flex-col gap-y-5 overflow-y-auto px-5 py-4 lg:fixed lg:flex lg:w-72 lg:px-6 lg:py-4",
+        )}
+      >
+        <div>
+          <LogoLink />
         </div>
-        <div
-          className={cn("min-h-screen max-w-4xl grow space-y-4 p-3 lg:ml-72")}
-        >
-          {children}
-        </div>
+        <Sidebar />
       </div>
-    );
-  }
-
-  return <SidebarLayout />;
+      <div className={cn("min-h-screen max-w-4xl grow space-y-4 p-3 lg:ml-72")}>
+        {children}
+      </div>
+    </div>
+  );
 }
