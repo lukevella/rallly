fdc0bf57db101de5a91850922fac7ee1eba7f857
Luke Vella <me@lukevella.com>
2024-05-13 09:51:41 +0800

    🎨 Show list of participants in two columns

--- apps/web/src/components/poll/mobile-poll/poll-option.tsx
+++ apps/web/src/components/poll/mobile-poll/poll-option.tsx
@@ -45,7 +45,7 @@ const PollOptionVoteSummary: React.FunctionComponent<{ optionId: string }> = ({
           {t("noVotes")}
         </div>
       ) : (
-        <div className="grid grid-cols-1 gap-2">
+        <div className="grid grid-cols-2 gap-2">
           <div className="col-span-1 space-y-2.5">
             {participantsWhoVotedYes.map(({ name }, i) => (
               <div key={i} className="flex">
