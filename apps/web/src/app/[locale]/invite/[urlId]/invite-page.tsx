dcb7157b86a7705e2009506040dff67eb370ac9f
Luke Vella <me@lukevella.com>
2024-05-16 17:36:48 +0800

    💄 Update manage alert

--- apps/web/src/app/[locale]/invite/[urlId]/invite-page.tsx
+++ apps/web/src/app/[locale]/invite/[urlId]/invite-page.tsx
@@ -22,10 +22,10 @@ const GoToApp = () => {
   }
 
   return (
-    <div className="border-primary-200 bg-primary-50/75 rounded-lg border p-2.5 text-sm lg:py-1 lg:pr-1">
-      <div className="flex items-center justify-between">
-        <div className="flex gap-2.5 sm:items-center">
-          <UserCircle2Icon className="text-primary-600 size-4" />
+    <div className="border-primary-200 bg-primary-50/75 rounded-lg border p-2.5 text-sm sm:p-1.5">
+      <div className="flex items-start justify-between sm:items-center">
+        <div className="flex gap-2.5 sm:items-center lg:px-2.5">
+          <UserCircle2Icon className="text-primary-600 hidden size-4 sm:block" />
           <div className="flex grow flex-col gap-x-2.5 sm:flex-row">
             <h4 className="text-primary-600 font-medium">
               <Trans i18nKey="eventHostTitle" defaults="Manage Access" />
@@ -39,7 +39,7 @@ const GoToApp = () => {
           </div>
         </div>
         <Link
-          className="text-primary-600 hover:bg-primary-100 active:bg-primary-200 inline-flex h-9 items-center gap-x-2.5 rounded-md px-3 font-medium"
+          className="text-primary-600 hover:bg-primary-200/50 active:bg-primary-200 inline-flex h-9 items-center gap-x-2.5 rounded-md px-3 font-medium"
           href={`/poll/${poll.id}`}
         >
           <Trans i18nKey="manage" />
