be34905064a3c2d27339a389fc6bb6a87a718105
RJ Trenchard <rjtrenchard@gmail.com>
2024-05-27 01:38:09 -0700

    add working tests

--- apps/heartbeat-test/tests/sample.spec.ts
+++ apps/heartbeat-test/tests/sample.spec.ts
@@ -5,11 +5,19 @@
  */
 import { test, expect } from '@playwright/test';
 
-// access demo page, expect the title to be "Polls"
-test('has title', async ({ page }) => {
+// access demo page, expect the title to be "" (blank string)
+// currently, I do not see a /demo path in the project
+test('/demo has title', async ({ page }) => {
     await page.goto('/demo');
 
     // Expect the title to contain the string "Polls".
-    await expect(page).toHaveTitle(/Polls/);
+    await expect(page).toHaveTitle("");
 });
 
+
+// access /polls page, expect the title to be "Polls"
+test('/polls page has title', async ({ page }) => {
+    await page.goto('/polls');
+
+    await expect(page).toHaveTitle(/Polls/);
+});
