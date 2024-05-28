dbdac4b9989bd85de3bac030c487ed6ff86ba305
RJ Trenchard <rjtrenchard@gmail.com>
2024-05-27 21:18:16 -0700

    change reporter to github when in CI mode, list otherwise

--- apps/heartbeat-test/heartbeat.config.ts
+++ apps/heartbeat-test/heartbeat.config.ts
@@ -21,7 +21,10 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: 'html',
+
+    // 'github' for GitHub Actions CI to generate annotations, plus a concise 'dot'
+    // default 'list' when running locally
+    reporter: process.env.CI ? 'github' : 'list',
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('/')`. */
