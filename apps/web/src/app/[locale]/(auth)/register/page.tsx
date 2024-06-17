093ed1152aedc6630bf1f07af460c5df1d1990c1
Luke Vella <me@lukevella.com>
2024-06-17 22:44:35 +0100

    🔒️ Hide link to login from registration (#1158)

--- apps/web/src/app/[locale]/(auth)/register/page.tsx
+++ apps/web/src/app/[locale]/(auth)/register/page.tsx
@@ -1,29 +1,9 @@
-import Link from "next/link";
-import { Trans } from "react-i18next/TransWithoutContext";
-
 import { RegisterForm } from "@/app/[locale]/(auth)/register/register-page";
 import { Params } from "@/app/[locale]/types";
 import { getTranslation } from "@/app/i18n";
-import { AuthCard } from "@/components/auth/auth-layout";
 
-export default async function Page({ params }: { params: Params }) {
-  const { t } = await getTranslation(params.locale);
-  return (
-    <div>
-      <AuthCard>
-        <RegisterForm />
-      </AuthCard>
-      <div className="mt-4 pt-4 text-center text-gray-500 sm:text-base">
-        <Trans
-          t={t}
-          i18nKey="alreadyRegistered"
-          components={{
-            a: <Link href="/login" className="text-link" />,
-          }}
-        />
-      </div>
-    </div>
-  );
+export default async function Page() {
+  return <RegisterForm />;
 }
 
 export async function generateMetadata({ params }: { params: Params }) {
