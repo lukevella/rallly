4c2e575c2af70bcc0e82a46947c35d8ed8ef71a6
Luke Vella <me@lukevella.com>
2024-06-18 18:49:22 +0100

    🔥 Remove unused toast code

--- apps/web/src/utils/trpc/config.ts
+++ apps/web/src/utils/trpc/config.ts
@@ -1,7 +1,6 @@
 import * as Sentry from "@sentry/browser";
 import { MutationCache } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
-import toast from "react-hot-toast";
 import superjson from "superjson";
 
 export const trpcConfig = {
@@ -21,9 +20,6 @@ export const trpcConfig = {
     },
     mutationCache: new MutationCache({
       onError: (error) => {
-        toast.error(
-          "Uh oh! Something went wrong. The issue has been logged and we'll fix it as soon as possible. Please try again later.",
-        );
         Sentry.captureException(error);
       },
     }),
