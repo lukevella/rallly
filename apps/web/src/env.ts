b631d0765d270cebc70d116c7154ce8fcc70b68c
Luke Vella <me@lukevella.com>
2024-07-05 10:33:30 +0100

    🔧 Skip env validation during docker build

--- apps/web/src/env.ts
+++ apps/web/src/env.ts
@@ -89,4 +89,5 @@ export const env = createEnv({
     NEXT_PUBLIC_POSTHOG_API_KEY: process.env.NEXT_PUBLIC_POSTHOG_API_KEY,
     NEXT_PUBLIC_POSTHOG_API_HOST: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
   },
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
 });
