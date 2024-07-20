891a03a1f6b15c2409eeb7517e2dbd7538821663
Luke Vella <me@lukevella.com>
2024-07-20 11:06:30 +0100

    Update posthog bootstrap code
    Call identify on each app load
    
    Signed-off-by: Luke Vella <me@lukevella.com>

--- apps/web/src/contexts/posthog.tsx
+++ apps/web/src/contexts/posthog.tsx
@@ -1,32 +1,61 @@
 "use client";
+import { usePathname, useSearchParams } from "next/navigation";
 import posthog from "posthog-js";
-import { PostHogProvider as Provider } from "posthog-js/react";
+import { PostHogProvider as Provider, usePostHog } from "posthog-js/react";
+import React from "react";
 import { useMount } from "react-use";
 
 import { useUser } from "@/components/user-provider";
+import { env } from "@/env";
 
 type PostHogProviderProps = React.PropsWithChildren;
 
+if (typeof window !== "undefined" && env.NEXT_PUBLIC_POSTHOG_API_KEY) {
+  posthog.init(env.NEXT_PUBLIC_POSTHOG_API_KEY, {
+    debug: true,
+    api_host: env.NEXT_PUBLIC_POSTHOG_API_HOST,
+    capture_pageview: false,
+    capture_pageleave: true,
+    disable_session_recording: true,
+    enable_heatmaps: false,
+    persistence: "memory",
+    autocapture: false,
+    opt_out_capturing_by_default: false,
+  });
+}
+
+function usePostHogPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const posthog = usePostHog();
+  const previousUrl = React.useRef<string | null>(null);
+  React.useEffect(() => {
+    // Track pageviews
+    if (pathname && posthog) {
+      let url = window.origin + pathname;
+      if (searchParams?.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      if (previousUrl.current !== url) {
+        posthog.capture("$pageview", {
+          $current_url: url,
+        });
+        previousUrl.current = url;
+      }
+    }
+  }, [pathname, searchParams, posthog]);
+}
+
 export function PostHogProvider(props: PostHogProviderProps) {
   const { user } = useUser();
+  usePostHogPageView();
 
   useMount(() => {
-    // initalize posthog with our user id
-    if (process.env.NEXT_PUBLIC_POSTHOG_API_KEY) {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_API_KEY, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_API_HOST,
-        opt_out_capturing_by_default: false,
-        capture_pageview: true,
-        capture_pageleave: true,
-        // we use session recordings to offer support and
-        // improve the product for our pro users
-        disable_session_recording: true,
-        persistence: "memory",
-        autocapture: false,
-        opt_in_site_apps: true,
-        bootstrap: {
-          distinctID: user.id,
-        },
+    if (user.email) {
+      posthog.identify(user.id);
+      posthog.people.set({
+        $email: user.email,
+        $name: user.name,
       });
     }
   });
