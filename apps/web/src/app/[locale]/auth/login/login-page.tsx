c524c53cc9f2c779d04599b27fe9699f457b40f7
Luke Vella <me@lukevella.com>
2024-07-31 20:15:38 +0100

    🐛 Use correct syntax for passing person properties (#1232)

--- apps/web/src/app/[locale]/auth/login/login-page.tsx
+++ apps/web/src/app/[locale]/auth/login/login-page.tsx
@@ -29,10 +29,8 @@ export const LoginPage = ({ magicLink, email }: PageProps) => {
         if (updatedSession) {
           // identify the user in posthog
           posthog?.identify(updatedSession.user.id, {
-            $set: {
-              email: updatedSession.user.email,
-              name: updatedSession.user.name,
-            },
+            email: updatedSession.user.email,
+            name: updatedSession.user.name,
           });
 
           await trpcUtils.invalidate();
