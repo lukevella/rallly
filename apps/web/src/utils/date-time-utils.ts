e2e7f14df641a4680f6801296cf4fa5eeca56f5f
Luke Vella <me@lukevella.com>
2024-08-04 13:13:47 +0100

    Update

--- apps/web/src/utils/date-time-utils.ts
+++ apps/web/src/utils/date-time-utils.ts
@@ -20,36 +20,58 @@ export function parseIanaTimezone(timezone: string): {
 
 export function getBrowserTimeZone() {
   const timeZone = dayjs.tz.guess();
-  return resolveGeographicTimeZone(timeZone);
+  return normalizeTimeZone(timeZone);
 }
 
-export function resolveGeographicTimeZone(timeZone: string) {
-  const tz = supportedTimeZones.find((tz) => tz === timeZone);
+function getTimeZoneOffset(timeZone: string) {
+  try {
+    return dayjs().tz(timeZone).utcOffset();
+  } catch (e) {
+    console.error(`Failed to resolve timezone ${timeZone}`);
+    return 0;
+  }
+}
+
+function isFixedOffsetTimeZone(timeZone: string) {
+  return (
+    timeZone.toLowerCase().startsWith("etc") ||
+    timeZone.toLowerCase().startsWith("gmt") ||
+    timeZone.toLowerCase().startsWith("utc")
+  );
+}
+
+/**
+ * Given a timezone, this function returns a normalized timezone
+ * that is supported by the application. If the timezone is not
+ * recognized, it will return a timezone in the same continent
+ * with the same offset.
+ * @param timeZone
+ * @returns
+ */
+export function normalizeTimeZone(timeZone: string) {
+  let tz = supportedTimeZones.find((tz) => tz === timeZone);
+
+  if (tz) {
+    return tz;
+  }
+
+  const timeZoneOffset = getTimeZoneOffset(timeZone);
+
+  if (!isFixedOffsetTimeZone(timeZone)) {
+    // Find a timezone in the same continent with the same offset
+    const [continent] = timeZone.split("/");
+    const sameContinentTimeZones = supportedTimeZones.filter((tz) =>
+      tz.startsWith(continent),
+    );
+    tz = sameContinentTimeZones.find((tz) => {
+      return dayjs().tz(tz, true).utcOffset() === timeZoneOffset;
+    });
+  }
 
   if (!tz) {
-    let timeZoneOffset = 0;
-    try {
-      timeZoneOffset = dayjs().tz(timeZone).utcOffset();
-    } catch (e) {
-      console.error(`Failed to resolve timezone ${timeZone}`);
-    }
-    const isGeographic =
-      timeZone.includes("/") && !timeZone.toLowerCase().startsWith("etc/");
-
-    if (isGeographic) {
-      // Find a timezone in the same continent with the same offset
-      const [continent] = timeZone.split("/");
-      const sameContinentTimeZones = supportedTimeZones.filter((tz) =>
-        tz.startsWith(continent),
-      );
-      return sameContinentTimeZones.find((tz) => {
-        return dayjs().tz(tz, true).utcOffset() === timeZoneOffset;
-      });
-    }
-
-    return supportedTimeZones.find((tz) => {
-      return dayjs().tz(tz, true).utcOffset() === timeZoneOffset;
-    })!;
+    tz = supportedTimeZones.find((tz) => {
+      return getTimeZoneOffset(tz) === timeZoneOffset;
+    })!; // We assume there has to be a timezone with the same offset
   }
 
   return tz;
--- apps/web/src/utils/date-time-utilts.test.ts
+++ apps/web/src/utils/date-time-utilts.test.ts
@@ -5,45 +5,39 @@ import { describe, expect, it } from "vitest";
 
 import { supportedTimeZones } from "@/utils/supported-time-zones";
 
-import { resolveGeographicTimeZone } from "./date-time-utils";
+import { normalizeTimeZone } from "./date-time-utils";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
-describe("resolveGeographicTimezone", () => {
+describe("Normalize Time Zone", () => {
   it("should return same timezone when given a geographic timezone", () => {
-    const browserTimeZone = resolveGeographicTimeZone("Europe/London");
+    const browserTimeZone = normalizeTimeZone("Europe/London");
 
     // Assert that the browser time zone is one of the supported time zones
     expect(browserTimeZone).toBe("Europe/London");
   });
   it("should return a supported timezone when given a fixed offset timezone", () => {
-    const browserTimeZone = resolveGeographicTimeZone("Etc/GMT-1");
+    const browserTimeZone = normalizeTimeZone("Etc/GMT-1");
 
     // Assert that the browser time zone is one of the supported time zones
     expect(supportedTimeZones.includes(browserTimeZone)).toBe(true);
   });
   it("should return a supported timezone when given GMT", () => {
-    const browserTimeZone = resolveGeographicTimeZone("GMT");
+    const browserTimeZone = normalizeTimeZone("GMT");
 
     // Assert that the browser time zone is one of the supported time zones
     expect(supportedTimeZones.includes(browserTimeZone)).toBe(true);
   });
   it("should return a supported timezone when given UTC", () => {
-    const browserTimeZone = resolveGeographicTimeZone("UTC");
+    const browserTimeZone = normalizeTimeZone("UTC");
 
     // Assert that the browser time zone is one of the supported time zones
     expect(supportedTimeZones.includes(browserTimeZone)).toBe(true);
   });
 
-  it("should return the same timezone when given a supported timezone", () => {
-    const browserTimeZone = resolveGeographicTimeZone("America/Ciudad_Juarez");
-
-    expect(browserTimeZone).toBe("America/Ciudad_Juarez");
-  });
-
-  it("should return a timezone in the same continent when not supported", () => {
-    const browserTimeZone = resolveGeographicTimeZone("Asia/Kolkata");
+  it("should return a valid timezone in the same continent when not recognized", () => {
+    const browserTimeZone = normalizeTimeZone("Asia/Kolkata");
 
     expect(browserTimeZone).toBe("Asia/Calcutta");
   });
