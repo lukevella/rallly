669422741a1a89325dc3fc948d7f5c4c0ee306cc
Luke Vella <me@lukevella.com>
2024-08-23 18:08:47 +0100

    ♻️ Use stripe client in billing package

--- packages/backend/stripe.ts
+++
@@ -1,8 +0,0 @@
-import Stripe from "stripe";
-
-export type { Stripe } from "stripe";
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
-  apiVersion: "2023-08-16",
-  typescript: true,
-});
