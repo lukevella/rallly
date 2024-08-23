8ca4b2acf8e9f0cb1f8b1429979f4493e5cef9d7
Luke Vella <me@lukevella.com>
2024-08-23 11:21:32 +0100

    Update layout
    Signed-off-by: Luke Vella <me@lukevella.com>

--- apps/web/src/app/[locale]/(admin)/layout.tsx
+++ apps/web/src/app/[locale]/(admin)/layout.tsx
@@ -5,7 +5,6 @@ import { MobileNavigation } from "@/app/[locale]/(admin)/mobile-navigation";
 import { ProBadge } from "@/app/[locale]/(admin)/pro-badge";
 import { Sidebar } from "@/app/[locale]/(admin)/sidebar";
 import { LogoLink } from "@/app/components/logo-link";
-import { PayWallDialog } from "@/components/pay-wall-dialog";
 
 export default async function Layout({
   children,
@@ -13,26 +12,30 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <PayWallDialog>
-      <div className="flex flex-col pb-16 md:pb-0">
-        <div
-          className={cn(
-            "fixed inset-y-0 z-50 hidden w-72 shrink-0 flex-col gap-y-4 overflow-y-auto p-6 md:flex",
-          )}
-        >
-          <div className="flex w-full items-center justify-between gap-4">
-            <LogoLink />
-            <ProBadge />
-          </div>
-          <Sidebar />
+    <div className="flex h-screen flex-col pb-16 md:pb-0">
+      <div
+        className={cn(
+          "fixed inset-y-0 z-50 hidden w-72 shrink-0 flex-col gap-y-4 overflow-y-auto p-6 py-5 md:flex",
+        )}
+      >
+        <div className="flex w-full items-center justify-between gap-4">
+          <LogoLink />
+          <ProBadge />
         </div>
-        <div className={cn("grow space-y-4 p-3 md:ml-72 md:p-4 lg:p-6")}>
-          <div className="max-w-5xl">{children}</div>
-        </div>
-        <div className="fixed bottom-0 z-20 flex h-16 w-full flex-col justify-center bg-gray-100/90 backdrop-blur-md md:hidden">
-          <MobileNavigation />
+        <Sidebar />
+      </div>
+      <div
+        className={cn(
+          "flex flex-1 flex-col pb-2 lg:min-w-0 lg:pl-72 lg:pr-2 lg:pt-2",
+        )}
+      >
+        <div className="grow p-6 lg:rounded-lg lg:bg-white lg:p-10 lg:shadow-sm lg:ring-1 lg:ring-gray-950/5 dark:lg:bg-gray-900 dark:lg:ring-white/10">
+          {children}
         </div>
       </div>
-    </PayWallDialog>
+      <div className="fixed bottom-0 z-20 flex h-16 w-full flex-col justify-center bg-gray-100/90 backdrop-blur-md md:hidden">
+        <MobileNavigation />
+      </div>
+    </div>
   );
 }
