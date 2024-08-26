9b5643e3203f551131bedda35beb8d5b87a00aa0
Niko Heller <hellerniko@gmail.com>
2024-08-26 10:41:11 +0200

    add locale as sendTemplate param and pass it to templates add locale to EmailContext

--- packages/emails/src/templates/_components/email-context.tsx
+++ packages/emails/src/templates/_components/email-context.tsx
@@ -2,10 +2,12 @@ export type EmailContext = {
   logoUrl: string;
   baseUrl: string;
   domain: string;
+  locale: string;
 };
 
 export const defaultEmailContext = {
   logoUrl: "https://rallly.co/logo.png",
   baseUrl: "https://rallly.co",
   domain: "rallly.co",
+  locale: "en",
 };
