df858523e2cb98436537a1a39ce5c876649d8c63
Luke Vella <me@lukevella.com>
2024-09-01 16:01:14 +0100

    Fix ts issues

--- apps/web/src/components/trans.tsx
+++ apps/web/src/components/trans.tsx
@@ -2,9 +2,15 @@ import { Trans as BaseTrans } from "react-i18next";
 
 import { useTranslation } from "@/app/i18n/client";
 
-type TransWithContextProps = Omit<React.ComponentProps<typeof BaseTrans>, "t">;
+import { I18nNamespaces } from "../../declarations/i18next";
 
-export const Trans = (props: TransWithContextProps) => {
-  const { t } = useTranslation();
-  return <BaseTrans t={t} {...props} />;
+export const Trans = (props: {
+  i18nKey: keyof I18nNamespaces["app"];
+  defaults?: string;
+  values?: Record<string, string | number | boolean | undefined>;
+  children?: React.ReactNode;
+  components?: Record<string, React.ReactElement> | React.ReactElement[];
+}) => {
+  const { t } = useTranslation("app");
+  return <BaseTrans ns="app" t={t} {...props} />;
 };
