be216344e2a49ad03518a4af9eece9980de74f2f
Luke Vella <me@lukevella.com>
2024-09-07 15:37:44 +0100

    🔥 Remove unused files and config (#1329)

--- apps/web/src/utils/prevent-widows.ts
+++
@@ -1,8 +0,0 @@
-export const preventWidows = (text: string) => {
-  // This only really makes sense if we have at least 3 words
-  if (text.split(" ").length < 3) {
-    return text;
-  }
-  const index = text.lastIndexOf(" ");
-  return [text.substring(0, index), text.substring(index + 1)].join("\u00a0");
-};
