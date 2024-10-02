6cfb56d4bc0b21d9770c28b4e82c65209cc296b9
Luke Vella <me@lukevella.com>
2024-10-02 17:00:22 +0100

    🔍️ Add sitemap (#1371)

---
+++ apps/landing/src/app/sitemap.ts
@@ -0,0 +1,54 @@
+import { supportedLngs } from "@rallly/languages";
+import type { MetadataRoute } from "next";
+
+import { getAllPosts } from "@/lib/api";
+import { absoluteUrl } from "@/utils/absolute-url";
+
+const alternateLanguages = supportedLngs.filter((lng) => lng !== "en");
+
+const getAlternateLanguages = (path: string) => {
+  return alternateLanguages.reduce<Record<string, string>>((acc, locale) => {
+    acc[locale] = absoluteUrl(`/${locale}${path}`);
+    return acc;
+  }, {});
+};
+
+export default async function Sitemap(): Promise<MetadataRoute.Sitemap> {
+  const posts = getAllPosts(["slug"]);
+
+  return [
+    {
+      url: absoluteUrl(),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+      alternates: {
+        languages: getAlternateLanguages("/"),
+      },
+    },
+    {
+      url: absoluteUrl("/pricing"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+      alternates: {
+        languages: getAlternateLanguages("/pricing"),
+      },
+    },
+    {
+      url: absoluteUrl("/blog"),
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+      alternates: {
+        languages: getAlternateLanguages("/blog"),
+      },
+    },
+    ...posts.map((post) => ({
+      url: absoluteUrl(`/blog/${post.slug}`),
+      lastModified: new Date(), // TODO: Update posts to include a lastModified date
+      changeFrequency: "yearly" as const,
+      priority: 0.7,
+    })),
+  ];
+}
