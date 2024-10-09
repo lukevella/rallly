8af0621d9e46c95687ffddc2cbda69cefe2d7a8b
Luke Vella <me@lukevella.com>
2024-10-09 17:47:46 +0100

    🐛 Fix name change

--- apps/web/src/app/[locale]/(admin)/settings/profile/profile-settings.tsx
+++ apps/web/src/app/[locale]/(admin)/settings/profile/profile-settings.tsx
@@ -12,10 +12,11 @@ import { useForm } from "react-hook-form";
 import { ProfilePicture } from "@/app/[locale]/(admin)/settings/profile/profile-picture";
 import { Trans } from "@/components/trans";
 import { useUser } from "@/components/user-provider";
+import { trpc } from "@/utils/trpc/client";
 
 export const ProfileSettings = () => {
   const { user, refresh } = useUser();
-
+  const changeName = trpc.user.changeName.useMutation();
   const form = useForm<{
     name: string;
     email: string;
@@ -32,7 +33,8 @@ export const ProfileSettings = () => {
       <Form {...form}>
         <form
           onSubmit={handleSubmit(async (data) => {
-            await refresh({ name: data.name });
+            await changeName.mutateAsync({ name: data.name });
+            await refresh();
             reset(data);
           })}
         >
