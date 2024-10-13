bfbbed82654f1d0e9270aea7db38a482370cd3d4
Luke Vella <me@lukevella.com>
2024-10-13 21:40:10 +0100

    Update vercel script

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-yarn prisma generate
-yarn build
+pnpm prisma generate
+pnpm build
 # Deploy migration using direct database connection (no connection pool)
-DATABASE_URL=$DIRECT_DATABASE_URL yarn db:deploy
+DATABASE_URL=$DIRECT_DATABASE_URL pnpm db:deploy
