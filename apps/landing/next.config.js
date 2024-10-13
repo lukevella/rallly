04257844b949cb7cbf27178b34cbad49015bbf25
Luke Vella <me@lukevella.com>
2024-10-13 18:49:14 +0100

    Remove sentry files from landing page

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,8 +1,3 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
@@ -92,6 +87,4 @@ const nextConfig = {
   },
 };
 
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
 module.exports = withBundleAnalyzer(nextConfig);
