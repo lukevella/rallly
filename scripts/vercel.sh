3b8600a2b8c1142ec35b277e70a55d5b34daf264
Luke Vella <me@lukevella.com>
2024-10-16 19:30:58 +0100

    Fix script

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-pnpm prisma generate
+pnpm db:generate
 pnpm build
 # Deploy migration using direct database connection (no connection pool)
 DATABASE_URL=$DIRECT_DATABASE_URL pnpm db:deploy
