c8cb3b38557cf7f9a0ea9d0e94e7945c419903de
Luke Vella <me@lukevella.com>
2024-10-20 20:54:27 +0100

    🐛 Fix can’t delete user with deleted polls

--- packages/database/index.ts
+++ packages/database/index.ts
@@ -3,19 +3,7 @@ import { PrismaClient } from "@prisma/client";
 export type * from "@prisma/client";
 
 const prismaClientSingleton = () => {
-  return new PrismaClient().$extends({
-    query: {
-      poll: {
-        findMany: ({ args, query }) => {
-          if (!args.where?.deleted) {
-            args.where = { ...args.where, deleted: false };
-          }
-
-          return query(args);
-        },
-      },
-    },
-  });
+  return new PrismaClient();
 };
 
 export type ExtendedPrismaClient = ReturnType<typeof prismaClientSingleton>;
