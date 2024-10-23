9672d379f86371c434e255c36b7097d6d404c11e
Simon <contact@smn.codes>
2024-10-23 18:47:50 +0200

    Fix time slot checkbox being pushed out of viewport

--- apps/web/src/components/poll/mobile-poll/time-slot-option.tsx
+++ apps/web/src/components/poll/mobile-poll/time-slot-option.tsx
@@ -1,6 +1,6 @@
 import { ClockIcon } from "lucide-react";
 import * as React from "react";
-
+import { createBreakpoint } from "react-use";
 import PollOption, { PollOptionProps } from "./poll-option";
 
 export interface TimeSlotOptionProps extends PollOptionProps {
@@ -9,20 +9,33 @@ export interface TimeSlotOptionProps extends PollOptionProps {
   duration: string;
 }
 
+const useBreakpoint = createBreakpoint({
+  hideDuration: 320,
+  showDuration: 380,
+});
+
 const TimeSlotOption: React.FunctionComponent<TimeSlotOptionProps> = ({
   startTime,
   endTime,
   duration,
   ...rest
 }) => {
+  const breakpoint = useBreakpoint();
+  const showVotes = !!(rest.selectedParticipantId || rest.editable);
+  const showDuration =
+    breakpoint === "showDuration" ||
+    (breakpoint !== "showDuration" && !showVotes);
+
   return (
     <PollOption {...rest}>
       <div className="flex items-center gap-x-4 text-sm">
         <div>{`${startTime} - ${endTime}`}</div>
-        <div className="flex items-center gap-x-1.5 opacity-50">
-          <ClockIcon className="size-4" />
-          {duration}
-        </div>
+        {showDuration && (
+          <div className="flex items-center gap-x-1.5 opacity-50">
+            <ClockIcon className="size-4" />
+            {duration}
+          </div>
+        )}
       </div>
     </PollOption>
   );
