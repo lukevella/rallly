77f80f6dfb85dbd80f2db5115cd4887c0570b93b
Luke Vella <me@lukevella.com>
2024-10-24 16:03:32 +0100

    Hide end time

--- apps/web/src/components/poll/mobile-poll/time-slot-option.tsx
+++ apps/web/src/components/poll/mobile-poll/time-slot-option.tsx
@@ -1,6 +1,8 @@
+import { cn } from "@rallly/ui";
+import { Icon } from "@rallly/ui/icon";
 import { ClockIcon } from "lucide-react";
 import * as React from "react";
-import { cn } from "@rallly/ui";
+
 import PollOption, { PollOptionProps } from "./poll-option";
 
 export interface TimeSlotOptionProps extends PollOptionProps {
@@ -11,24 +13,22 @@ export interface TimeSlotOptionProps extends PollOptionProps {
 
 const TimeSlotOption: React.FunctionComponent<TimeSlotOptionProps> = ({
   startTime,
-  endTime,
   duration,
   ...rest
 }) => {
-  const showVotes = !!(rest.selectedParticipantId || rest.editable);
-
   return (
     <PollOption {...rest}>
       <div className="flex items-center gap-x-4 text-sm">
-        <div>{`${startTime} - ${endTime}`}</div>
+        <div>{startTime}</div>
         <div
           className={cn(
-            "flex items-center gap-x-1.5 opacity-50",
-            { hidden: showVotes },
+            "flex items-center gap-x-1.5 whitespace-nowrap opacity-50",
             "xs:flex",
           )}
         >
-          <ClockIcon className="size-4" />
+          <Icon>
+            <ClockIcon />
+          </Icon>
           {duration}
         </div>
       </div>
