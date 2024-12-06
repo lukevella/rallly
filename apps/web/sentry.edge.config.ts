1844eb312f90db38d38950349407f50ec5bc5ed2
Luke Vella <me@lukevella.com>
2024-12-06 22:49:06 +0000

    🔧 Reduce sentry sample rate

--- apps/web/sentry.edge.config.ts
+++ apps/web/sentry.edge.config.ts
@@ -10,7 +10,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   dsn: SENTRY_DSN,
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.2,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
