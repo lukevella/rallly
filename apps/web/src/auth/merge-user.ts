1dc19835854117bf15ec1874252476bb1100056e
Luke Vella <me@lukevella.com>
2024-12-24 13:30:42 +0100

    Peform user merge in transaction

--- apps/web/src/auth/merge-user.ts
+++ apps/web/src/auth/merge-user.ts
@@ -4,39 +4,43 @@ export const mergeGuestsIntoUser = async (
   userId: string,
   guestIds: string[],
 ) => {
-  await prisma.poll.updateMany({
-    where: {
-      guestId: {
-        in: guestIds,
-      },
-    },
-    data: {
-      guestId: null,
-      userId: userId,
-    },
-  });
+  return await prisma.$transaction(async (tx) => {
+    await Promise.all([
+      tx.poll.updateMany({
+        where: {
+          guestId: {
+            in: guestIds,
+          },
+        },
+        data: {
+          guestId: null,
+          userId: userId,
+        },
+      }),
 
-  await prisma.participant.updateMany({
-    where: {
-      guestId: {
-        in: guestIds,
-      },
-    },
-    data: {
-      guestId: null,
-      userId: userId,
-    },
-  });
+      tx.participant.updateMany({
+        where: {
+          guestId: {
+            in: guestIds,
+          },
+        },
+        data: {
+          guestId: null,
+          userId: userId,
+        },
+      }),
 
-  await prisma.comment.updateMany({
-    where: {
-      guestId: {
-        in: guestIds,
-      },
-    },
-    data: {
-      guestId: null,
-      userId: userId,
-    },
+      tx.comment.updateMany({
+        where: {
+          guestId: {
+            in: guestIds,
+          },
+        },
+        data: {
+          guestId: null,
+          userId: userId,
+        },
+      }),
+    ]);
   });
 };
