4f4e7f99c60e3941fb9db96374be209094e9712d
Luke Vella <me@lukevella.com>
2024-12-26 12:37:27 +0100

    Get correct count on dashboard

--- apps/web/src/trpc/routers/dashboard.ts
+++ apps/web/src/trpc/routers/dashboard.ts
@@ -6,7 +6,13 @@ export const dashboard = router({
   info: possiblyPublicProcedure.query(async ({ ctx }) => {
     const activePollCount = await prisma.poll.count({
       where: {
-        userId: ctx.user.id,
+        ...(ctx.user.isGuest
+          ? {
+              guestId: ctx.user.id,
+            }
+          : {
+              userId: ctx.user.id,
+            }),
         status: "live",
         deleted: false, // TODO (Luke Vella) [2024-06-16]: We should add deleted/cancelled to the status enum
       },
