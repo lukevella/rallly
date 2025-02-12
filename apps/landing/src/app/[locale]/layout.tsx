cb27ae9ea769ee22802bc220228902edc41c7122
Luke Vella <me@lukevella.com>
2025-02-12 14:39:14 +0700

    ♿️ Make view port scalable (#1560)

--- apps/landing/src/app/[locale]/layout.tsx
+++ apps/landing/src/app/[locale]/layout.tsx
@@ -31,8 +31,6 @@ export async function generateStaticParams() {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 };
 
 export default async function Root({
