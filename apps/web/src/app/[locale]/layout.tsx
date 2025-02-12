5818bef02c70d0d60a764e3a4f1952b381b133b0
Luke Vella <me@lukevella.com>
2025-02-12 13:33:18 +0700

    ♿️ Make view port scalable

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -19,8 +19,6 @@ const inter = Inter({
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 1,
-  userScalable: false,
 };
 
 export default async function Root({
