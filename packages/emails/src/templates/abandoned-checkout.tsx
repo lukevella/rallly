17d386d905f6fa2bedd6686998d3589009c11d5a
Luke Vella <me@lukevella.com>
2025-02-12 11:42:39 +0700

    🌐  Make discount value a parameter (#1557)

--- packages/emails/src/templates/abandoned-checkout.tsx
+++ packages/emails/src/templates/abandoned-checkout.tsx
@@ -7,12 +7,16 @@ import type { EmailContext } from "../types";
 
 interface AbandonedCheckoutEmailProps {
   recoveryUrl: string;
+  discount: number;
+  couponCode: string;
   name?: string;
   ctx: EmailContext;
 }
 
 export const AbandonedCheckoutEmail = ({
   recoveryUrl,
+  discount,
+  couponCode,
   name,
   ctx,
 }: AbandonedCheckoutEmailProps) => {
@@ -22,7 +26,8 @@ export const AbandonedCheckoutEmail = ({
       poweredBy={false}
       preview={ctx.t("abandoned_checkout_preview", {
         defaultValue:
-          "Exclusive offer: Get 20% off your first year of Rallly Pro!",
+          "Exclusive offer: Get {{discount}}% off your first year of Rallly Pro!",
+        discount,
         ns: "emails",
       })}
     >
@@ -68,7 +73,7 @@ export const AbandonedCheckoutEmail = ({
           defaults="To help you get started, I'd like to offer you <b>{{discount}}% off your first year</b> with Rallly Pro. Simply use this code during checkout:"
           ns="emails"
           values={{
-            discount: 20,
+            discount,
           }}
           components={{
             b: <b />,
@@ -84,7 +89,7 @@ export const AbandonedCheckoutEmail = ({
               fontWeight: "bold",
             }}
           >
-            GETPRO1Y20
+            {couponCode}
           </Text>
         </Card>
         <Button href={recoveryUrl} id="recoveryUrl">
@@ -131,7 +136,8 @@ AbandonedCheckoutEmail.getSubject = (
   return (
     "🎉 " +
     ctx.t("abandoned_checkout_subject", {
-      defaultValue: "Get 20% off your first year of Rallly Pro",
+      defaultValue: "Get {{discount}}% off your first year of Rallly Pro",
+      discount: props.discount,
       ns: "emails",
     })
   );
