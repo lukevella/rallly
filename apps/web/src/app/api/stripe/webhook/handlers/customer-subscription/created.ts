965e73130388d76356c9f6e53badf7f786bcbac9
Luke Vella <me@lukevella.com>
2025-02-23 15:43:56 +0000

    Handle subscription creation in single transaction

--- apps/web/src/app/api/stripe/webhook/handlers/customer-subscription/created.ts
+++ apps/web/src/app/api/stripe/webhook/handlers/customer-subscription/created.ts
@@ -24,29 +24,26 @@ export async function onCustomerSubscriptionCreated(event: Stripe.Event) {
     throw new Error("Missing user ID");
   }
 
-  // Create the subscription in the database
-  await prisma.subscription.create({
-    data: {
-      id: subscription.id,
-      active: isActive,
-      priceId,
-      currency,
-      interval,
-      amount,
-      status: subscription.status,
-      createdAt: toDate(subscription.created),
-      periodStart: toDate(subscription.current_period_start),
-      periodEnd: toDate(subscription.current_period_end),
-    },
-  });
-
-  // Update the user with the subscription id
+  // Create and update user
   await prisma.user.update({
     where: {
       id: res.data.userId,
     },
     data: {
-      subscriptionId: subscription.id,
+      subscription: {
+        create: {
+          id: subscription.id,
+          active: isActive,
+          priceId,
+          currency,
+          interval,
+          amount,
+          status: subscription.status,
+          createdAt: toDate(subscription.created),
+          periodStart: toDate(subscription.current_period_start),
+          periodEnd: toDate(subscription.current_period_end),
+        },
+      },
     },
   });
 }
