5d9e14b94e920024370ef2b7322f97b14cde816e
Luke Vella <me@lukevella.com>
2025-02-24 17:20:02 +0000

    ♻️ Update and move empty state component (#1573)

--- apps/web/src/app/[locale]/(admin)/polls/user-polls.tsx
+++ apps/web/src/app/[locale]/(admin)/polls/user-polls.tsx
@@ -19,7 +19,7 @@ import {
   EmptyStateDescription,
   EmptyStateIcon,
   EmptyStateTitle,
-} from "@/app/components/empty-state";
+} from "@/components/empty-state";
 import { PollStatusBadge } from "@/components/poll-status";
 import { Spinner } from "@/components/spinner";
 import { Trans } from "@/components/trans";
