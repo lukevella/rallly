65cc51a0fbe68a9453073feae0c953011529ed29
Luke Vella <me@lukevella.com>
2025-02-24 15:24:34 +0000

    🗃️ Use subscription status enum

---
+++ packages/database/prisma/migrations/20250221160003_set_subscription_status_enum/migration.sql
@@ -0,0 +1,24 @@
+ALTER TYPE "subscription_status" ADD VALUE 'incomplete';
+ALTER TYPE "subscription_status" ADD VALUE 'incomplete_expired';
+ALTER TYPE "subscription_status" ADD VALUE 'canceled';
+ALTER TYPE "subscription_status" ADD VALUE 'unpaid';
+
+-- AlterTable
+ALTER TABLE "subscriptions" 
+ADD COLUMN "status_enum" "subscription_status";
+
+-- Migrate existing data
+UPDATE "subscriptions" 
+SET "status_enum" = "status"::"subscription_status";
+
+-- Make the new column required
+ALTER TABLE "subscriptions" 
+ALTER COLUMN "status_enum" SET NOT NULL;
+
+-- Drop the old column
+ALTER TABLE "subscriptions" 
+DROP COLUMN "status";
+
+-- Rename the new column
+ALTER TABLE "subscriptions"
+RENAME COLUMN "status_enum" TO "status";
