55489290d416b96d0e349db2be6a80936ea18cd5
Luke Vella <me@lukevella.com>
2025-02-27 11:06:51 +0000

    ♻️ Make user required in subscription model

---
+++ packages/database/prisma/migrations/20250227110115_make_subscription_user_required/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `subscription_id` on the `users` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[user_id]` on the table `subscriptions` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `user_id` to the `subscriptions` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "users" DROP CONSTRAINT "users_subscription_id_fkey";
+
+-- DropIndex
+DROP INDEX "users_subscription_id_key";
+
+-- AlterTable
+ALTER TABLE "subscriptions" ADD COLUMN     "user_id" TEXT;
+
+-- Populate user_id in subscriptions table using data from users table
+UPDATE "subscriptions" s
+SET "user_id" = u.id
+FROM "users" u
+WHERE u."subscription_id" = s.id;
+
+-- Make user_id NOT NULL after populating data
+ALTER TABLE "subscriptions" ALTER COLUMN "user_id" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "subscription_id";
+
+-- CreateIndex
+CREATE UNIQUE INDEX "subscriptions_user_id_key" ON "subscriptions"("user_id");
+
+-- AddForeignKey
+ALTER TABLE "subscriptions" ADD CONSTRAINT "subscriptions_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
