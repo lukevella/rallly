19d65750d35aa00416865947932804c4ed4c5af0
Luke Vella <me@lukevella.com>
2025-03-27 20:13:11 +0000

    Update test

--- apps/web/src/app/api/house-keeping/delete-inactive-polls/route.ts
+++ apps/web/src/app/api/house-keeping/delete-inactive-polls/route.ts
@@ -16,9 +16,6 @@ export async function POST() {
   // Mark inactive polls as deleted in a single query
   const { count: markedDeleted } = await prisma.poll.updateMany({
     where: {
-      touchedAt: {
-        lt: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // 30 days ago
-      },
       deleted: false,
       options: {
         none: {
@@ -28,12 +25,39 @@ export async function POST() {
         },
       },
       // Include polls without a user or with users that don't have an active subscription
-      OR: [
-        { userId: null },
+      AND: [
         {
-          user: {
-            OR: [{ subscription: null }, { subscription: { active: false } }],
-          },
+          OR: [
+            { userId: null },
+            {
+              user: {
+                OR: [
+                  { subscription: null },
+                  { subscription: { active: false } },
+                ],
+              },
+            },
+          ],
+        },
+        {
+          OR: [
+            // Check if there are no views OR all views are older than 30 days
+            {
+              views: {
+                none: {
+                  viewedAt: {
+                    gte: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // 30 days ago
+                  },
+                },
+              },
+            },
+            // Check if touchedAt is older than 30 days
+            {
+              touchedAt: {
+                lt: new Date(Date.now() - 30 * 24 * 60 * 60 * 1000), // 30 days ago
+              },
+            },
+          ],
         },
       ],
     },
