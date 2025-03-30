8163abe1863e93a3054545a072063cbfc420a6b7
Luke Vella <me@lukevella.com>
2025-03-30 21:57:35 +0100

    🔥 Remove references to sentry on landing page

--- apps/landing/next.config.js
+++ apps/landing/next.config.js
@@ -1,8 +1,3 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
