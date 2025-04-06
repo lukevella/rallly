4aa03c38daf9c932350d9c1064165db7a623720b
Luke Vella <me@lukevella.com>
2025-04-06 19:52:49 +0100

    Update skeleton

--- apps/web/src/app/components/page-layout.tsx
+++ apps/web/src/app/components/page-layout.tsx
@@ -78,7 +78,7 @@ export function PageSkeleton() {
   return (
     <PageContainer>
       <PageHeader>
-        <Skeleton className="size-8" />
+        <Skeleton className="mb-3 size-8" />
         <PageTitle>
           <Skeleton className="h-8 w-32" />
         </PageTitle>
