12651abd0c0d7c7204e4873a8e259b1fd7821fea
Luke Vella <me@lukevella.com>
2025-04-16 19:51:02 +0100

    🐛 Fix invalid-session route

--- apps/web/src/app/api/auth/invalid-session/route.ts
+++ apps/web/src/app/api/auth/invalid-session/route.ts
@@ -1,7 +1,12 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
 import { signOut } from "@/next-auth";
 
-export async function GET() {
-  return await signOut({
-    redirectTo: "/",
+export async function GET(req: NextRequest) {
+  await signOut({
+    redirect: false,
   });
+
+  return NextResponse.redirect(new URL("/login", req.url));
 }
