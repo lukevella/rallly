e20b04a2596c9e50f1525818337e18388d9e6523
Luke Vella <me@lukevella.com>
2025-04-17 20:53:35 +0100

    🐛 Fix loader

--- apps/web/src/app/[locale]/poll/[urlId]/loading.tsx
+++ apps/web/src/app/[locale]/poll/[urlId]/loading.tsx
@@ -1,3 +1,9 @@
+import { Spinner } from "@/components/spinner";
+
 export default function Loading() {
-  return null;
+  return (
+    <div className="flex h-96 items-center justify-center">
+      <Spinner />
+    </div>
+  );
 }
