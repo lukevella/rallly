dab71e5f63ddbdfb54f313cf6ebfe376ad82f6a4
Luke Vella <me@lukevella.com>
2025-04-29 16:02:45 +0100

    Use base url to make cookie secure

--- apps/web/src/app/[locale]/(auth)/register/actions.ts
+++ apps/web/src/app/[locale]/(auth)/register/actions.ts
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 export async function setToken(token: string) {
   (await cookies()).set("registration-token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: process.env.NEXT_PUBLIC_BASE_URL?.startsWith("https://"),
     sameSite: "lax",
     maxAge: 15 * 60,
   });
