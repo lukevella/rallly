1b3b3aac5024520eb6a0445299fa175244b74c0e
Luke Vella <me@lukevella.com>
2025-05-23 22:27:20 +0100

    ♻️ Clean up login code (#1727)

--- apps/web/src/app/[locale]/(auth)/login/actions.ts
+++ apps/web/src/app/[locale]/(auth)/login/actions.ts
@@ -1,27 +1,14 @@
 "use server";
 
-import { prisma } from "@rallly/database";
 import { cookies } from "next/headers";
 
 export async function setVerificationEmail(email: string) {
-  const user = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-    select: {
-      email: true,
-    },
+  const cookieStore = await cookies();
+
+  cookieStore.set("verification-email", email, {
+    httpOnly: true,
+    secure: process.env.NEXT_PUBLIC_BASE_URL?.startsWith("https://"),
+    sameSite: "lax",
+    maxAge: 15 * 60,
   });
-
-  if (user) {
-    (await cookies()).set("verification-email", user.email, {
-      httpOnly: true,
-      secure: process.env.NEXT_PUBLIC_BASE_URL?.startsWith("https://"),
-      sameSite: "lax",
-      maxAge: 15 * 60,
-    });
-    return true;
-  }
-
-  return false;
 }
