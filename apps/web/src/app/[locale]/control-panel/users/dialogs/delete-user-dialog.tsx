1536d2451bc37870a98ef8b43a12b953210ec665
Luke Vella <me@lukevella.com>
2025-05-24 13:41:11 +0100

    Update

--- apps/web/src/app/[locale]/control-panel/users/dialogs/delete-user-dialog.tsx
+++ apps/web/src/app/[locale]/control-panel/users/dialogs/delete-user-dialog.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "@/i18n/client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@rallly/ui/button";
 import {
+  Dialog,
   DialogClose,
   DialogContent,
   DialogDescription,
@@ -22,6 +23,7 @@ import {
 } from "@rallly/ui/form";
 import { Input } from "@rallly/ui/input";
 import { useRouter } from "next/navigation";
+import { useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import { deleteUser } from "../actions";
@@ -43,9 +45,17 @@ const useSchema = (email: string) => {
 export function DeleteUserDialog({
   userId,
   email,
-}: { userId: string; email: string }) {
+  open,
+  onOpenChange,
+}: {
+  userId: string;
+  email: string;
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}) {
   const router = useRouter();
   const schema = useSchema(email);
+  const [isPending, startTransition] = useTransition();
   const form = useForm({
     resolver: zodResolver(schema),
     defaultValues: {
@@ -53,58 +63,67 @@ export function DeleteUserDialog({
     },
   });
   return (
-    <DialogContent>
-      <DialogHeader>
-        <DialogTitle>
-          <Trans i18nKey="deleteUser" defaults="Delete User" />
-        </DialogTitle>
-        <DialogDescription>
-          <Trans
-            i18nKey="areYouSureYouWantToDeleteThisUser"
-            defaults="Are you sure you want to delete this user?"
-          />
-        </DialogDescription>
-      </DialogHeader>
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(async (data) => {
-            await deleteUser({ userId, email: data.email });
-            router.refresh();
-          })}
-        >
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  <Trans i18nKey="email" defaults="Email" />
-                </FormLabel>
-                <FormControl>
-                  <Input placeholder={email} {...field} />
-                </FormControl>
-                <FormDescription>
-                  <Trans
-                    i18nKey="typeTheEmailAddressOfTheUserYouWantToDelete"
-                    defaults="Type the email address of the user you want to delete."
-                  />
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <DialogFooter className="mt-6">
-            <DialogClose asChild>
-              <Button>
-                <Trans i18nKey="cancel" defaults="Cancel" />
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            <Trans i18nKey="deleteUserTitle" defaults="Delete User" />
+          </DialogTitle>
+          <DialogDescription>
+            <Trans
+              i18nKey="deleteUserDesc"
+              defaults="Are you sure you want to delete this user?"
+            />
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(async (data) => {
+              startTransition(async () => {
+                await deleteUser({ userId, email: data.email });
+                router.refresh();
+                onOpenChange(false);
+              });
+            })}
+          >
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    <Trans i18nKey="email" defaults="Email" />
+                  </FormLabel>
+                  <FormControl>
+                    <Input placeholder={email} {...field} />
+                  </FormControl>
+                  <FormDescription>
+                    <Trans
+                      i18nKey="deleteUserHint"
+                      defaults="Type <b>{email}</b> to delete this user."
+                      values={{ email }}
+                      components={{
+                        b: <b className="whitespace-nowrap font-normal" />,
+                      }}
+                    />
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter className="mt-6">
+              <DialogClose asChild>
+                <Button>
+                  <Trans i18nKey="cancel" defaults="Cancel" />
+                </Button>
+              </DialogClose>
+              <Button variant="destructive" loading={isPending} type="submit">
+                <Trans i18nKey="delete" defaults="Delete" />
               </Button>
-            </DialogClose>
-            <Button variant="destructive" type="submit">
-              <Trans i18nKey="delete" defaults="Delete" />
-            </Button>
-          </DialogFooter>
-        </form>
-      </Form>
-    </DialogContent>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 }
--- apps/web/src/app/[locale]/control-panel/users/user-row.tsx
+++ apps/web/src/app/[locale]/control-panel/users/user-row.tsx
@@ -2,24 +2,29 @@
 import { OptimizedAvatarImage } from "@/components/optimized-avatar-image";
 import { StackedListItem } from "@/components/stacked-list";
 import { Trans } from "@/components/trans";
-import { Badge } from "@rallly/ui/badge";
+import { useUser } from "@/components/user-provider";
+import { userRoleSchema } from "@/features/user/schema";
+import { cn } from "@rallly/ui";
 import { Button } from "@rallly/ui/button";
+import { useDialog } from "@rallly/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
   DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@rallly/ui/dropdown-menu";
 import { Icon } from "@rallly/ui/icon";
-import {
-  CircleArrowDownIcon,
-  CircleArrowUpIcon,
-  MoreHorizontal,
-  TrashIcon,
-} from "lucide-react";
+import { MoreHorizontal, TrashIcon, UserPenIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
-import { changeRole, deleteUser } from "./actions";
+import { useTransition } from "react";
+import { changeRole } from "./actions";
+import { DeleteUserDialog } from "./dialogs/delete-user-dialog";
 
 export function UserRow({
   name,
@@ -35,62 +40,85 @@ export function UserRow({
   role: "admin" | "user";
 }) {
   const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const { user } = useUser();
+  const deleteDialog = useDialog();
+
+  const isYou = userId === user?.id;
   return (
-    <StackedListItem>
-      <div>
-        <OptimizedAvatarImage src={image} name={name} size="md" />
-      </div>
-      <div className="flex-1 min-w-0">
-        <div className="font-semibold">{name}</div>
-        <div className="text-muted-foreground truncate">{email}</div>
-      </div>
-      <div className="flex items-center gap-4">
-        <Badge className="capitalize">{role}</Badge>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon">
-              <Icon>
-                <MoreHorizontal />
-              </Icon>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            {role === "user" && (
+    <>
+      <StackedListItem
+        className={cn({
+          "opacity-50": isPending,
+        })}
+      >
+        <div>
+          <OptimizedAvatarImage src={image} name={name} size="md" />
+        </div>
+        <div className="flex-1 min-w-0">
+          <div className="font-semibold">{name}</div>
+          <div className="text-muted-foreground truncate">{email}</div>
+        </div>
+        <div className="flex items-center gap-4">
+          <span className="capitalize">{role}</span>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Icon>
+                  <MoreHorizontal />
+                </Icon>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuSub>
+                <DropdownMenuSubTrigger disabled={isYou}>
+                  <Icon>
+                    <UserPenIcon />
+                  </Icon>
+                  <Trans i18nKey="changeRole" defaults="Change role" />
+                </DropdownMenuSubTrigger>
+                <DropdownMenuSubContent>
+                  <DropdownMenuRadioGroup
+                    value={role}
+                    onValueChange={async (value) => {
+                      startTransition(async () => {
+                        await changeRole({
+                          role: userRoleSchema.parse(value),
+                          userId,
+                        });
+                        router.refresh();
+                      });
+                    }}
+                  >
+                    <DropdownMenuRadioItem value="admin">
+                      <Trans i18nKey="admin" defaults="Admin" />
+                    </DropdownMenuRadioItem>
+                    <DropdownMenuRadioItem value="user">
+                      <Trans i18nKey="user" defaults="User" />
+                    </DropdownMenuRadioItem>
+                  </DropdownMenuRadioGroup>
+                </DropdownMenuSubContent>
+              </DropdownMenuSub>
+              <DropdownMenuSeparator />
               <DropdownMenuItem
+                className="text-destructive"
                 onClick={async () => {
-                  await changeRole({ userId, role: "admin" });
-                  router.refresh();
+                  deleteDialog.trigger();
                 }}
+                disabled={isYou}
               >
-                <CircleArrowUpIcon className="size-4" />
-                <Trans i18nKey="promoteToAdmin" defaults="Promote to Admin" />
+                <TrashIcon className="size-4" />
+                <Trans i18nKey="delete" defaults="Delete" />
               </DropdownMenuItem>
-            )}
-            {role === "admin" && (
-              <DropdownMenuItem
-                onClick={async () => {
-                  await changeRole({ userId, role: "user" });
-                  router.refresh();
-                }}
-              >
-                <CircleArrowDownIcon className="size-4" />
-                <Trans i18nKey="demoteToUser" defaults="Demote to User" />
-              </DropdownMenuItem>
-            )}
-            <DropdownMenuSeparator />
-            <DropdownMenuItem
-              className="text-destructive"
-              onClick={async () => {
-                await deleteUser({ userId, email });
-                router.refresh();
-              }}
-            >
-              <TrashIcon className="size-4" />
-              <Trans i18nKey="delete" defaults="Delete" />
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-    </StackedListItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </StackedListItem>
+      <DeleteUserDialog
+        {...deleteDialog.dialogProps}
+        userId={userId}
+        email={email}
+      />
+    </>
   );
 }
