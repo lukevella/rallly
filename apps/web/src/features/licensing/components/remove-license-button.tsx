97eadfbeaffaf4a66d7fcdb4c61d3703232d3172
Luke Vella <me@lukevella.com>
2025-05-24 14:46:19 +0100

    Use dialog confirmation

--- apps/web/src/features/licensing/components/remove-license-button.tsx
+++ apps/web/src/features/licensing/components/remove-license-button.tsx
@@ -2,6 +2,17 @@
 
 import { Trans } from "@/components/trans";
 import { Button } from "@rallly/ui/button";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  useDialog,
+} from "@rallly/ui/dialog";
 import { Icon } from "@rallly/ui/icon";
 import { XIcon } from "lucide-react";
 import { useRouter } from "next/navigation";
@@ -15,22 +26,52 @@ export function RemoveLicenseButton({
 }) {
   const [isPending, startTransition] = useTransition();
   const router = useRouter();
+  const dialog = useDialog();
   return (
-    <Button
-      loading={isPending}
-      onClick={() =>
-        startTransition(async () => {
-          await removeInstanceLicense({
-            licenseId,
-          });
-          router.refresh();
-        })
-      }
-    >
-      <Icon>
-        <XIcon />
-      </Icon>
-      <Trans i18nKey="removeLicense" defaults="Remove License" />
-    </Button>
+    <Dialog {...dialog.dialogProps}>
+      <DialogTrigger asChild>
+        <Button onClick={() => dialog.trigger()}>
+          <Icon>
+            <XIcon />
+          </Icon>
+          <Trans i18nKey="removeLicense" defaults="Remove License" />
+        </Button>
+      </DialogTrigger>
+      <DialogContent size="sm">
+        <DialogHeader>
+          <DialogTitle>
+            <Trans i18nKey="removeLicense" defaults="Remove License" />
+          </DialogTitle>
+          <DialogDescription>
+            <Trans
+              i18nKey="removeLicenseDescription"
+              defaults="Are you sure you want to remove this license?"
+            />
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter>
+          <DialogClose asChild>
+            <Button>
+              <Trans i18nKey="cancel" defaults="Cancel" />
+            </Button>
+          </DialogClose>
+          <Button
+            loading={isPending}
+            variant="destructive"
+            onClick={() =>
+              startTransition(async () => {
+                await removeInstanceLicense({
+                  licenseId,
+                });
+                router.refresh();
+                dialog.dismiss();
+              })
+            }
+          >
+            <Trans i18nKey="removeLicense" defaults="Remove License" />
+          </Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   );
 }
