9d601381e5016d9709ecf866296dcad9e1aa8dd1
Luke Vella <me@lukevella.com>
2025-06-04 20:49:19 +0100

    Update

--- packages/ui/src/action-bar.tsx
+++ packages/ui/src/action-bar.tsx
@@ -19,10 +19,10 @@ const ActionBar = React.forwardRef<
   return (
     <DialogPrimitive.Root modal={false} open={open} onOpenChange={onOpenChange}>
       <DialogPrimitive.Content
+        forceMount={true}
         ref={ref}
-        onOpenAutoFocus={(e) => e.preventDefault()}
         className={cn(
-          "-translate-x-1/2 pointer-events-auto fixed bottom-8 left-1/2 z-50 inline-flex items-center gap-4 rounded-xl border bg-action-bar p-2 text-action-bar-foreground shadow-lg transition-transform duration-200 ease-out data-[state=closed]:translate-y-full data-[state=open]:translate-y-0 data-[state=closed]:opacity-0 data-[state=open]:opacity-100 md:bottom-16 md:left-[calc(50vw+var(--sidebar-width)/2)]",
+          "-translate-x-1/2 fixed bottom-3 left-1/2 z-50 inline-flex w-[calc(100%-24px)] flex-col items-center gap-2 rounded-xl bg-action-bar p-2 text-action-bar-foreground shadow-lg transition-transform duration-200 ease-out data-[state=closed]:pointer-events-none data-[state=closed]:translate-y-full data-[state=open]:translate-y-0 data-[state=closed]:opacity-0 data-[state=open]:opacity-100 md:bottom-16 md:left-[calc(50vw+var(--sidebar-width)/2)] md:w-fit md:flex-row",
           className,
         )}
         {...props}
@@ -41,7 +41,7 @@ const ActionBarTitle = React.forwardRef<
   <DialogPrimitive.Title
     ref={ref}
     className={cn(
-      "flex flex-1 items-center whitespace-nowrap px-2.5",
+      "flex flex-1 items-center whitespace-nowrap p-2.5",
       className,
     )}
     {...props}
@@ -57,7 +57,7 @@ const ActionBarGroup = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("inline-flex items-center gap-2", className)}
+    className={cn("flex w-full flex-col gap-2 md:w-fit md:flex-row", className)}
     {...props}
   />
 ));
