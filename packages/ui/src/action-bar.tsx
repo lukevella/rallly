a171312eb4cf6bec97b05f4d4c30e3f6e861103a
Luke Vella <me@lukevella.com>
2025-06-05 11:07:07 +0100

    Simplify action bar position

--- packages/ui/src/action-bar.tsx
+++ packages/ui/src/action-bar.tsx
@@ -22,8 +22,8 @@ const ActionBar = React.forwardRef<
         forceMount={true}
         ref={ref}
         className={cn(
-          "-translate-x-1/2 fixed bottom-3 left-1/2 z-50 inline-flex flex-col items-center gap-2 rounded-xl bg-action-bar p-2 text-action-bar-foreground shadow-lg transition-transform duration-200 ease-out data-[state=closed]:pointer-events-none data-[state=closed]:translate-y-full data-[state=open]:translate-y-0 data-[state=closed]:opacity-0 data-[state=open]:opacity-100",
-          "md:bottom-16 md:left-[calc(50vw+var(--sidebar-width)/2)] md:w-fit md:flex-row",
+          "-translate-x-1/2 fixed bottom-3 z-50 flex items-start gap-2 rounded-xl bg-action-bar p-2 text-action-bar-foreground shadow-lg transition-transform duration-200 ease-out data-[state=closed]:pointer-events-none data-[state=closed]:translate-y-full data-[state=open]:translate-y-0 data-[state=closed]:opacity-0 data-[state=open]:opacity-100",
+          "left-1/2 md:bottom-16 md:w-fit",
           "w-[calc(100%-24px)]",
           className,
         )}
@@ -42,7 +42,7 @@ const ActionBarTitle = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <DialogPrimitive.Title
     ref={ref}
-    className={cn("flex flex-1 items-center p-2.5 text-sm", className)}
+    className={cn("flex flex-1 items-center px-2.5 py-2 text-sm", className)}
     {...props}
   >
     {children}
@@ -56,7 +56,7 @@ const ActionBarGroup = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <div
     ref={ref}
-    className={cn("flex w-full flex-col gap-2 md:w-fit md:flex-row", className)}
+    className={cn("flex justify-end gap-2", className)}
     {...props}
   />
 ));
