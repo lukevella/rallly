0827e7030a62de7b4ce9ecc3fd7b9fdbbe2d83d3
Luke Vella <me@lukevella.com>
2025-07-10 16:05:35 +0100

    Remove unecessary index

--- packages/database/prisma/migrations/20250710102819_add_active_space_to_user/migration.sql
+++ packages/database/prisma/migrations/20250710102819_add_active_space_to_user/migration.sql
@@ -1,9 +1,6 @@
 -- AlterTable
 ALTER TABLE "users" ADD COLUMN     "active_space_id" TEXT;
 
--- CreateIndex
-CREATE INDEX "users_active_space_id_idx" ON "users"("active_space_id");
-
 -- AddForeignKey
 ALTER TABLE "users" ADD CONSTRAINT "users_active_space_id_fkey" FOREIGN KEY ("active_space_id") REFERENCES "spaces"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
