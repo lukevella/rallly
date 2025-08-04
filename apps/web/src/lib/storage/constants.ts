fb730aaa9bf0990e2049efd06b2893b79dd0e8ce
Luke Vella <me@lukevella.com>
2025-08-04 15:18:43 +0100

    Fix boolean logic

--- apps/web/src/lib/storage/constants.ts
+++ apps/web/src/lib/storage/constants.ts
@@ -1,4 +1,5 @@
 import { env } from "@/env";
 
-export const isStorageEnabled =
-  !env.S3_BUCKET_NAME || !env.S3_ACCESS_KEY_ID || !env.S3_SECRET_ACCESS_KEY;
+export const isStorageEnabled = Boolean(
+  env.S3_BUCKET_NAME && env.S3_ACCESS_KEY_ID && env.S3_SECRET_ACCESS_KEY,
+);
