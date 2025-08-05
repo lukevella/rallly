eedc4a4a363b21d2f2917d386112eaef6d633e42
Luke Vella <me@lukevella.com>
2025-08-05 17:23:50 +0100

    Remove console.log

--- apps/web/src/features/poll/helpers.ts
+++ apps/web/src/features/poll/helpers.ts
@@ -13,19 +13,16 @@ export async function canUserManagePoll(
   },
 ) {
   if (user.isGuest) {
-    console.log("user is guest");
     // guest user is owner
     return poll.guestId === user.id;
   }
 
   if (poll.userId && !user.isGuest && poll.userId === user.id) {
-    console.log("user is owner");
     // user is owner
     return true;
   }
 
   if (poll.spaceId && !user.isGuest) {
-    console.log("checking if space member");
     const space = await prisma.spaceMember.findUnique({
       where: {
         spaceId_userId: {
