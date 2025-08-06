994b3cedf98461c566f50f787b3c8361204261af
Luke Vella <me@lukevella.com>
2025-08-06 12:27:08 +0100

    ✨ Handle when user is not a member of any space

--- apps/web/src/app/[locale]/(space)/layout.tsx
+++ apps/web/src/app/[locale]/(space)/layout.tsx
@@ -1,15 +1,9 @@
-import { redirect } from "next/navigation";
 import { requireSpace, requireUser } from "@/auth/data";
-import { isUserOnboarded } from "@/features/setup/utils";
 import { SpaceProvider } from "@/features/space/client";
 import { TimezoneProvider } from "@/lib/timezone/client/context";
 
 async function loadData() {
   const [user, space] = await Promise.all([requireUser(), requireSpace()]);
-
-  if (!isUserOnboarded(user)) {
-    redirect("/setup");
-  }
 
   return {
     user,
