ec2d7d9634534e02a535d631e9c97088f9a38aad
Luke Vella <me@lukevella.com>
2025-08-07 13:56:46 +0100

    💄 Update logout icon

--- apps/web/src/app/[locale]/account/profile/logout-button.tsx
+++ apps/web/src/app/[locale]/account/profile/logout-button.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@rallly/ui/button";
+import { Icon } from "@rallly/ui/icon";
 import { LogOutIcon } from "lucide-react";
 import { signOut } from "next-auth/react";
 import { Trans } from "@/components/trans";
@@ -13,7 +14,9 @@ export function LogoutButton() {
       }}
       variant="default"
     >
-      <LogOutIcon className="size-4" />
+      <Icon>
+        <LogOutIcon />
+      </Icon>
       <Trans i18nKey="signOut" defaults="Sign Out" />
     </Button>
   );
