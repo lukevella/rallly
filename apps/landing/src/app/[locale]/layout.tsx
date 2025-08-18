84c3fb577c013ec6c482cbef1b74d07f47ded274
Luke Vella <me@lukevella.com>
2025-08-18 17:27:14 +0100

    Check if process.env.NEXT_PUBLIC_BASE_URL is set

--- apps/landing/src/app/[locale]/layout.tsx
+++ apps/landing/src/app/[locale]/layout.tsx
@@ -46,5 +46,7 @@ export default async function Root(props: { children: React.ReactNode }) {
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL as string),
+  metadataBase: process.env.NEXT_PUBLIC_BASE_URL
+    ? new URL(process.env.NEXT_PUBLIC_BASE_URL)
+    : undefined,
 };
