3028c602e6b09b945115b80fea9961036d00bca6
Luke Vella <me@lukevella.com>
2025-08-18 16:09:10 +0100

    Update

--- apps/landing/src/middleware.ts
+++ apps/landing/src/middleware.ts
@@ -1,31 +1,28 @@
 import { getPreferredLocale } from "@rallly/languages/get-preferred-locale";
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-import { fallbackLng, headerName, languages } from "@/i18n/settings";
+import { headerName, languages } from "@/i18n/settings";
 
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
+  const newUrl = req.nextUrl.clone();
 
   // Check if the language is already in the path
   const localeInPath = languages.find(
     (loc) => pathname === `/${loc}` || pathname.startsWith(`/${loc}/`),
   );
+
   const preferredLocale = getPreferredLocale({
     acceptLanguageHeader: req.headers.get("accept-language") ?? undefined,
   });
 
-  const headers = new Headers(req.headers);
-  headers.set(headerName, localeInPath || preferredLocale);
+  const locale = localeInPath || preferredLocale;
 
-  // If the language is not in the path, redirect to include it
+  const headers = new Headers(req.headers);
+  headers.set(headerName, locale);
+
   if (!localeInPath) {
-    const newUrl = new URL(
-      `/${preferredLocale}${req.nextUrl.pathname}${req.nextUrl.search}`,
-      req.url,
-    );
-    if (preferredLocale === fallbackLng) {
-      return NextResponse.rewrite(newUrl);
-    }
+    newUrl.pathname = `/${locale}${pathname}`;
     return NextResponse.redirect(newUrl);
   }
 
