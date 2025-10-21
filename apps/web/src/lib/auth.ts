b7202740ccc9112dacda6b334deeed9d2bd8195e
Luke Vella <me@lukevella.com>
2025-10-21 18:25:04 +0100

    Set appName

--- apps/web/src/lib/auth.ts
+++ apps/web/src/lib/auth.ts
@@ -90,6 +90,7 @@ if (env.OIDC_CLIENT_ID && env.OIDC_CLIENT_SECRET && env.OIDC_DISCOVERY_URL) {
 }
 
 export const authLib = betterAuth({
+  appName: "Rallly",
   secret: env.SECRET_PASSWORD,
   database: prismaAdapter(prisma, {
     provider: "postgresql",
