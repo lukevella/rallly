dd25e52c95feda52ca97de6d6fb97b703bff910d
Luke Vella <me@lukevella.com>
2025-11-11 17:25:51 +0000

    ♻️ Refactor posthog shutdown call (#2002)

--- apps/web/src/app/api/auth/[...nextauth]/route.ts
+++ apps/web/src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,4 @@
 import { handlers } from "@/next-auth";
-import { withPosthog } from "@/utils/posthog";
 
-export const GET = withPosthog(handlers.GET);
-export const POST = withPosthog(handlers.POST);
+export const GET = handlers.GET;
+export const POST = handlers.POST;
