ee28e22195d6fce26f48eca1101e8fa25309b3ce
Luke Vella <me@lukevella.com>
2025-11-17 15:57:49 +0000

    Fix

--- apps/web/src/features/poll/query.ts
+++ apps/web/src/features/poll/query.ts
@@ -1,10 +1,13 @@
 import { prisma } from "@rallly/database";
 
 export const hasPollAdminAccess = async (pollId: string, userId: string) => {
-  const poll = await prisma.poll.findUnique({
+  const poll = await prisma.poll.findFirst({
     where: {
       id: pollId,
       OR: [{ userId: userId }, { space: { members: { some: { userId } } } }],
+    },
+    select: {
+      id: true,
     },
   });
 
