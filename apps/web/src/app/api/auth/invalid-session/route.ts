71d195a1251ac5e0304f3530a8d1c5409397900c
Luke Vella <me@lukevella.com>
2025-11-27 22:46:11 +0000

    🔊 Log error when signing out

--- apps/web/src/app/api/auth/invalid-session/route.ts
+++ apps/web/src/app/api/auth/invalid-session/route.ts
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 import { signOut } from "@/lib/auth";
 
 export async function GET(req: NextRequest) {
-  await signOut();
+  try {
+    await signOut();
+  } catch (error) {
+    console.error("FAILED TO SIGN OUT", error);
+  }
 
   return NextResponse.redirect(new URL("/login", req.url));
 }
