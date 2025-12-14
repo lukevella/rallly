be074a4c6869a3e413f3e36c52aed7b739274d71
Luke Vella <me@lukevella.com>
2025-12-14 22:04:48 +0000

    🐛 Fix invalid-session redirect error

--- apps/web/src/app/api/auth/invalid-session/route.ts
+++ apps/web/src/app/api/auth/invalid-session/route.ts
@@ -1,10 +1,18 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
+import auth from "@/lib/auth";
 import { signOut as legacySignOut } from "@/next-auth";
 
 export async function GET(req: NextRequest) {
   try {
-    await legacySignOut();
+    await Promise.all([
+      legacySignOut({
+        redirect: false,
+      }),
+      auth.api.signOut({
+        headers: req.headers,
+      }),
+    ]);
   } catch (error) {
     console.error("FAILED TO SIGN OUT", error);
   }
