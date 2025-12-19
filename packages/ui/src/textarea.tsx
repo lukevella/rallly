59fadcd0a4ebe913009f92db1947d5cd9e57c0d6
Luke Vella <me@lukevella.com>
2025-12-19 15:20:39 +0100

    🐛 Fix text area font size

--- packages/ui/src/textarea.tsx
+++ packages/ui/src/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "field-sizing-content flex min-h-16 w-full rounded-lg border border-input bg-input-background px-2 py-2 text-base shadow-xs outline-none transition-[color,box-shadow] placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 md:text-sm dark:bg-input/30 dark:aria-invalid:ring-destructive/40",
+          "field-sizing-content flex min-h-16 w-full rounded-lg border border-input bg-input-background px-2 py-2 text-sm shadow-xs outline-none transition-[color,box-shadow] placeholder:text-muted-foreground focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:cursor-not-allowed disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:bg-input/30 dark:aria-invalid:ring-destructive/40",
         )}
         ref={ref}
         {...props}
