9dbb0ff127866efe8664260eb3956943a9381a60
Luke Vella <me@lukevella.com>
2025-12-27 19:27:05 +0100

    Update colors

--- apps/web/src/app/[locale]/(auth)/login/components/or-divider.tsx
+++ apps/web/src/app/[locale]/(auth)/login/components/or-divider.tsx
@@ -4,9 +4,9 @@ export async function OrDivider() {
   const { t } = await getTranslation();
   return (
     <div className="flex items-center gap-x-2.5">
-      <hr className="grow border-gray-100" />
+      <hr className="grow border-gray-100 dark:border-gray-700" />
       <div className="text-muted-foreground lowercase">{t("or")}</div>
-      <hr className="grow border-gray-100" />
+      <hr className="grow border-gray-100 dark:border-gray-700" />
     </div>
   );
 }
