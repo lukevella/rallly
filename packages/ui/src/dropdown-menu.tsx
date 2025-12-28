0161c61655220bed2d52741264932880ecf6a9e9
Luke Vella <me@lukevella.com>
2025-12-28 10:58:58 +0100

    Update dropdown separator color

--- packages/ui/src/dropdown-menu.tsx
+++ packages/ui/src/dropdown-menu.tsx
@@ -196,7 +196,7 @@ const DropdownMenuSeparator = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DropdownMenuPrimitive.Separator
     ref={ref}
-    className={cn("-mx-1 my-1 h-px bg-muted", className)}
+    className={cn("-mx-1 my-1 h-px bg-popover-border", className)}
     {...props}
   />
 ));
