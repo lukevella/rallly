d0b5b81ba7d3b993c341ac24544259e2a72f8e7a
Luke Vella <me@lukevella.com>
2025-12-29 13:42:10 +0100

    💄 Update avatar component to use initials

--- apps/web/src/components/poll/desktop-poll/participant-row.tsx
+++ apps/web/src/components/poll/desktop-poll/participant-row.tsx
@@ -48,9 +48,9 @@ export const ParticipantRowView: React.FunctionComponent<{
     >
       <td
         style={{ minWidth: 235, maxWidth: 235 }}
-        className="sticky left-0 z-10 h-12 bg-background px-4"
+        className="sticky left-0 z-10 h-12 bg-background px-3"
       >
-        <div className="flex max-w-full items-center justify-between gap-x-2">
+        <div className="flex max-w-full items-center justify-between gap-x-1">
           <Participant>
             <OptimizedAvatarImage size="sm" name={name} />
             <ParticipantName>{name}</ParticipantName>
