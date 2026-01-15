a9cc7151e244d73a931bd6cf24a39c3efa5637f3
Luke Vella <me@lukevella.com>
2026-01-15 18:06:00 +0000

    💄 Update blog post styling on mobile

--- apps/landing/src/components/blog/post-header.tsx
+++ apps/landing/src/components/blog/post-header.tsx
@@ -8,9 +8,7 @@ type Props = {
 const PostHeader = ({ title, date }: Props) => {
   return (
     <header>
-      <h1 className="mb-2 text-center font-bold text-4xl tracking-tighter md:text-left md:leading-tight">
-        {title}
-      </h1>
+      <h1 className="mb-2 font-bold text-4xl tracking-tighter">{title}</h1>
       <div className="mb-2 text-center text-gray-400 text-lg sm:text-left">
         <DateFormatter dateString={date} />
       </div>
