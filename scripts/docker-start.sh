1108916381802c269ce6dd0fa7089bb3ee249e7a
Luke Vella <me@lukevella.com>
2026-02-12 11:27:15 +0000

    Keep AUTH_URL

--- scripts/docker-start.sh
+++ scripts/docker-start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 set -e
 
+# Needed by next-auth to verify the origin of the request
+export AUTH_URL=$NEXT_PUBLIC_BASE_URL
+
 npx prisma migrate deploy --config=./prisma.config.ts
 node apps/web/server.js
