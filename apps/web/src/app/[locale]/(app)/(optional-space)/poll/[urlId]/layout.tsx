1f2cc63e86b8d0154d0a67a93a38d1323cc298e2
Luke Vella <me@lukevella.com>
2026-02-22 15:05:09 +0000

    🐛 Fix crash when accessing poll that doesn’t exist anymore (#2218)

--- apps/web/src/app/[locale]/(app)/(optional-space)/poll/[urlId]/layout.tsx
+++ apps/web/src/app/[locale]/(app)/(optional-space)/poll/[urlId]/layout.tsx
@@ -2,9 +2,17 @@ import { prisma } from "@rallly/database";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import type { Metadata } from "next";
 import { notFound, redirect } from "next/navigation";
+import { cache } from "react";
 
 import { PollLayout } from "@/components/layouts/poll-layout";
 import { createAuthenticatedSSRHelper } from "@/trpc/server/create-ssr-helper";
+
+const getPoll = cache((urlId: string) =>
+  prisma.poll.findUnique({
+    where: { id: urlId },
+    select: { title: true },
+  }),
+);
 
 export default async function Layout(
   props: React.PropsWithChildren<{ params: Promise<{ urlId: string }> }>,
@@ -15,6 +23,12 @@ export default async function Layout(
 
   const trpc = await createAuthenticatedSSRHelper();
 
+  const p = await getPoll(params.urlId);
+
+  if (!p) {
+    notFound();
+  }
+
   // Prefetch all queries used in PollLayout
   const [poll] = await Promise.all([
     trpc.polls.get.fetch({ urlId: params.urlId }),
@@ -22,10 +36,6 @@ export default async function Layout(
     trpc.polls.getWatchers.prefetch({ pollId: params.urlId }),
     trpc.polls.comments.list.prefetch({ pollId: params.urlId }),
   ]);
-
-  if (!poll) {
-    notFound();
-  }
 
   if (!poll.adminUrlId) {
     redirect(`/invite/${params.urlId}`);
@@ -42,10 +52,7 @@ export async function generateMetadata(props: {
   params: Promise<{ locale: string; urlId: string }>;
 }): Promise<Metadata> {
   const params = await props.params;
-  const poll = await prisma.poll.findUnique({
-    where: { id: params.urlId },
-    select: { title: true },
-  });
+  const poll = await getPoll(params.urlId);
 
   if (!poll) {
     notFound();
