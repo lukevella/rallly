1e5f7d15069d62b954fa001af66d75d45f3bef37
Luke Vella <me@lukevella.com>
2026-03-10 00:24:11 +0000

    ♻️ Refactor checkout complete hook

--- apps/web/src/app/api/stripe/webhook/handlers/checkout/completed.ts
+++ apps/web/src/app/api/stripe/webhook/handlers/checkout/completed.ts
@@ -5,84 +5,80 @@ import { licenseCheckoutMetadataSchema } from "@/features/licensing/schema";
 import { licenseManager } from "@/features/licensing/server";
 import { getEmailClient } from "@/utils/emails";
 
-async function handleSelfHostedCheckoutSessionCompleted(
-  checkoutSession: Stripe.Checkout.Session,
-) {
-  const { success, data } = licenseCheckoutMetadataSchema.safeParse(
-    checkoutSession.metadata,
-  );
-
-  if (!success) {
-    // If there is no metadata than this is likely a donation from a payment link
-    console.info("No metadata found for session: ", checkoutSession.id);
-    return;
-  }
-
-  const { licenseType, version, seats } = data;
-
-  const customerDetails = checkoutSession.customer_details;
-
-  if (!customerDetails) {
-    throw new Error(
-      `No customer details found for session: ${checkoutSession.id}`,
-    );
-  }
-
-  const { email } = customerDetails;
-
-  if (!email) {
-    throw new Error(
-      `No email found for customer details in session: ${checkoutSession.id}`,
-    );
-  }
-
-  const license = await licenseManager.createLicense({
-    type: licenseType,
-    licenseeEmail: email,
-    licenseeName: customerDetails.name ?? undefined,
-    version,
-    seats,
-  });
-
-  posthog()?.capture({
-    distinctId: email,
-    event: "license_purchase",
-    properties: {
-      licenseType,
-      seats,
-      version,
-      $set: {
-        tier: licenseType,
-      },
-    },
-  });
-
-  if (!license || !license.data) {
-    throw new Error(
-      `Failed to create license for session: ${checkoutSession.id} - ${license?.error}`,
-    );
-  }
-
-  const emailClient = await getEmailClient();
-
-  await emailClient.sendTemplate("LicenseKeyEmail", {
-    to: email,
-    from: {
-      name: "Luke from Rallly",
-      address: env.SUPPORT_EMAIL,
-    },
-    props: {
-      licenseKey: license.data.key,
-      seats,
-      tier: licenseType,
-    },
-  });
-}
-
 export async function onCheckoutSessionCompleted(event: Stripe.Event) {
   const checkoutSession = event.data.object as Stripe.Checkout.Session;
 
   if (checkoutSession.subscription === null) {
-    await handleSelfHostedCheckoutSessionCompleted(checkoutSession);
+    const { success, data } = licenseCheckoutMetadataSchema.safeParse(
+      checkoutSession.metadata,
+    );
+
+    if (!success) {
+      // If there is no metadata than this is likely a donation from a payment link
+      console.info("No metadata found for session: ", checkoutSession.id);
+      return;
+    }
+
+    const { licenseType, version, seats } = data;
+
+    const customerDetails = checkoutSession.customer_details;
+
+    if (!customerDetails) {
+      throw new Error(
+        `No customer details found for session: ${checkoutSession.id}`,
+      );
+    }
+
+    const { email } = customerDetails;
+
+    if (!email) {
+      throw new Error(
+        `No email found for customer details in session: ${checkoutSession.id}`,
+      );
+    }
+
+    const license = await licenseManager.createLicense({
+      type: licenseType,
+      licenseeEmail: email,
+      licenseeName: customerDetails.name ?? undefined,
+      version,
+      seats,
+    });
+
+    if (!license || !license.data) {
+      throw new Error(
+        `Failed to create license for session: ${checkoutSession.id} - ${license?.error}`,
+      );
+    }
+
+    const emailClient = await getEmailClient();
+
+    await emailClient.sendTemplate("LicenseKeyEmail", {
+      to: email,
+      from: {
+        name: "Luke from Rallly",
+        address: env.SUPPORT_EMAIL,
+      },
+      props: {
+        licenseKey: license.data.key,
+        seats,
+        tier: licenseType,
+      },
+    });
+
+    posthog()?.capture({
+      uuid: event.id,
+      distinctId: email,
+      event: "license_purchase",
+      properties: {
+        licenseType,
+        seats,
+        version,
+        eventId: event.id,
+        $set: {
+          tier: licenseType,
+        },
+      },
+    });
   }
 }
