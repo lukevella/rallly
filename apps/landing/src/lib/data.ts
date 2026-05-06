50dc8e5ff9190fdbd22dc87e75b8f7019f2a2087
Luke Vella <me@lukevella.com>
2026-05-06 16:26:40 -0300

    Set cache profile

--- apps/landing/src/lib/data.ts
+++ apps/landing/src/lib/data.ts
@@ -1,9 +1,11 @@
 import "server-only";
 
 import { prisma } from "@rallly/database";
+import { cacheLife } from "next/cache";
 
 export const getUserCount = async () => {
   "use cache";
+  cacheLife("days");
   const userCount = await prisma.user.count({
     where: {
       isAnonymous: false,
