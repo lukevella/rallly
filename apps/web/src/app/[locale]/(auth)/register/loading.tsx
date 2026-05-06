350fb29a06c03bb3fed9a3df6001610fff31f037
Luke Vella <me@lukevella.com>
2026-05-06 16:04:18 -0300

    ♻️ Move loading component for auth route group

--- apps/web/src/app/[locale]/(auth)/register/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Spinner } from "@/components/spinner";
-
-export default function Loading() {
-  return (
-    <div className="flex h-full items-center justify-center">
-      <Spinner className="text-muted-foreground" />
-    </div>
-  );
-}
