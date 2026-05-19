f8b52c26d8c8f2a6c05467b8fb33e366d6d6de41
Luke Vella <me@lukevella.com>
2026-05-19 13:06:49 +0100

    ✨ Add sheets/slots/bookings tRPC router + signupSheets flag (RAL-1190) (#2392)

--- apps/web/src/lib/feature-flags/types.ts
+++ apps/web/src/lib/feature-flags/types.ts
@@ -6,6 +6,7 @@ export interface FeatureFlagConfig {
   registration: boolean;
   calendars: boolean;
   eventTypes: boolean;
+  signupSheets: boolean;
 }
 
 export type Feature = keyof FeatureFlagConfig;
---
+++ packages/database/prisma/migrations/20260519111112_invitee_email_citext/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "scheduled_event_invites" ALTER COLUMN "invitee_email" SET DATA TYPE CITEXT;
