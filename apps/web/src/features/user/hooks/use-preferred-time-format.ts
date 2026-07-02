ba724b4f94e79e8f7ac4e0d6e2671e82b5146318
Luke Vella <me@lukevella.com>
2026-07-02 10:21:42 +0100

    ♻️ Simplify usePreferredTimeFormat

--- apps/web/src/features/user/hooks/use-preferred-time-format.ts
+++ apps/web/src/features/user/hooks/use-preferred-time-format.ts
@@ -1,11 +1,8 @@
 "use client";
 
 import { useUser } from "@/components/user-provider";
-import { getLocaleDefaults } from "@/lib/datetime/locales";
-import { useLocale } from "@/lib/locale/client";
 
 export function usePreferredTimeFormat() {
   const { user } = useUser();
-  const { locale } = useLocale();
-  return user?.timeFormat ?? getLocaleDefaults(locale).timeFormat;
+  return user?.timeFormat;
 }
